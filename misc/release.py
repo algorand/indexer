55374e9ce451c7b9fd141df7e01d132d0ddad023
Brian Olson <brian.olson@algorand.com>
2020-04-10 18:27:20 +0000

    usage note. convert md to html for tar dist

--- misc/release.py
+++ misc/release.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
+#
+# usage:
+# python3 -m venv ve3
+# ve3/bin/pip install markdown2
+# ve3/bin/python misc/release.py
 
+import io
 import logging
 import os
 import subprocess
 import tarfile
+import time
+
+import markdown2
 
 logger = logging.getLogger(__name__)
 
@@ -30,7 +39,7 @@ filespec = [
         '',
     ],
     [
-        ['LICENSE', 'README.md'],
+        ['LICENSE'],
         '',
         None,
         '',
@@ -100,6 +109,15 @@ def build_deb(debarch, version):
         ['dpkg-deb', '--build', '.deb_tmp', debname])
     return debname
 
+_usage_html = None
+def usage_html():
+    global _usage_html
+    if _usage_html is not None:
+        return _usage_html
+    md = open('usage.md').read()
+    _usage_html = markdown2.markdown(md, extras=["tables", "fenced-code-blocks"])
+    return _usage_html
+
 def build_tar(goos, goarch, version):
     rootdir = 'algorand-indexer_{}_{}_{}'.format(goos, goarch, version)
     tarname = rootdir + '.tar.bz2'
@@ -111,6 +129,13 @@ def build_tar(goos, goarch, version):
             if tar_path:
                 os.makedirs(tar_path, exist_ok=True)
             tf.add(os.path.join(source_path, fname), os.path.join(rootdir, tar_path, fname))
+    ti = tarfile.TarInfo("usage.html")
+    ti.mtime = time.time()
+    ti.mode = 0o444
+    ti.type = tarfile.REGTYPE
+    uhtml = usage_html().encode('utf-8')
+    ti.size=len(uhtml)
+    tf.addfile(ti, io.BytesIO(uhtml))
     tf.close()
     return tarname
 
