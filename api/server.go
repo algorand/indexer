70ae160474bd9c3bb7fb34551a6a4d4359896e0f
Will Winder <wwinder.unh@gmail.com>
2020-04-30 12:46:28 -0400

    Remove GPL headers.

--- api/server.go
+++ api/server.go
@@ -1,19 +1,3 @@
-// Copyright (C) 2019-2020 Algorand, Inc.
-// This file is part of the Algorand Indexer
-//
-// Algorand Indexer is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// Algorand Indexer is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with Algorand Indexer.  If not, see <https://www.gnu.org/licenses/>.
-
 package api
 
 import (
@@ -44,7 +28,7 @@ func Serve(ctx context.Context, serveAddr string, db idb.IndexerDb, log *log.Log
 	e.Use(middlewares.MakeLogger(log))
 	e.Use(middleware.CORS())
 
-	if (len(tokens) > 0) {
+	if len(tokens) > 0 {
 		e.Use(middlewares.MakeAuth("X-Indexer-API-Token", tokens))
 	}
 
--- importer/importer.go
+++ importer/importer.go
@@ -1,19 +1,3 @@
-// Copyright (C) 2019-2020 Algorand, Inc.
-// This file is part of the Algorand Indexer
-//
-// Algorand Indexer is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// Algorand Indexer is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with Algorand Indexer.  If not, see <https://www.gnu.org/licenses/>.
-
 package importer
 
 import (
@@ -56,7 +40,6 @@ func init() {
 	TypeEnumMap = util.EnumListToMap(typeEnumList)
 	TypeEnumString = util.KeysStringInt(TypeEnumMap)
 }
-
 
 func keysString(m map[string]bool) string {
 	keys := make([]string, 0, len(m))
