ebe01e71c828ffe10a75b3baa4a5fe8512044533
Will Winder <wwinder.unh@gmail.com>
2020-05-12 20:32:47 -0400

    Fix spec, remove need for sed call, sort v3 file.

--- api/generate.sh
+++ api/generate.sh
@@ -6,9 +6,9 @@ pushd $rootdir
 
 # Convert v2 to v3
 curl -s -X POST "https://converter.swagger.io/api/convert" -H "accept: application/json" -H "Content-Type: application/json" -d @./indexer.oas2.json  -o 3.json
-cat 3.json | json_pp > indexer.oas3.yml
+
+python3 -c "import json; import sys; json.dump(json.load(sys.stdin), sys.stdout, indent=2, sort_keys=True)" < 3.json > indexer.oas3.yml
 rm 3.json
-sed -i 's/\*.\*/application\/json/g' indexer.oas3.yml
 
 echo "generating code."
 oapi-codegen -package generated -type-mappings integer=uint64 -generate types -o generated/types.go indexer.oas3.yml
