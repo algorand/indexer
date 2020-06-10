08d846da5019a57131a48486cc7de22e4ba62121
Benjamin Toll <benjam72@yahoo.com>
2020-06-09 23:39:58 -0400

    Add --indexer-bin location to e2e test command

--- build/e2e.sh
+++ build/e2e.sh
@@ -12,5 +12,5 @@ dpkg -i "./packages/$VERSION/algorand-indexer_${VERSION}_amd64.deb"
 sudo -u postgres bash -c "psql -c \"CREATE ROLE $USER WITH SUPERUSER CREATEDB LOGIN ENCRYPTED PASSWORD '$USER';\""
 sudo -u postgres bash -c "psql -c \"CREATE DATABASE $DB_NAME;\""
 
-python3 misc/e2etest.py --connection-string "host=localhost port=$PORT dbname=$DB_NAME sslmode=disable user=$USER password=$USER"
+python3 misc/e2etest.py --connection-string "host=localhost port=$PORT dbname=$DB_NAME sslmode=disable user=$USER password=$USER" --indexer-bin /usr/bin/algorand-indexer
 
