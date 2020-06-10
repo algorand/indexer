8a5c0d7cb7b557428c1c516486ed3af4209e3b7d
Benjamin Toll <benjam72@yahoo.com>
2020-06-10 12:16:46 -0400

    Added stage-releases-page mule task

--- mule/generate_releases_page.sh
+++ mule/generate_releases_page.sh
@@ -2,8 +2,12 @@
 
 set -ex
 
+ARCH=$(./mule/scripts/archtype.sh)
+OS_TYPE=$(./mule/scripts/ostype.sh)
+VERSION=$(./mule/scripts/compute_build_number.sh)
+WORKSPACE=$(pwd)
+
 git clone https://github.com/btoll/releases-page.git /tmp/releases-page
 cd /tmp/releases-page
-./generate_releases_page.py >| /tmp/index.html
-#mule -f package-deploy.yaml package-deploy-releases-page
+./generate_releases_page.py >| "$WORKSPACE/tmp/node_pkgs/$OS_TYPE/$ARCH/$VERSION/releases.html"
 
