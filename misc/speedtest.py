f3fe86bad6a950957453eacb00b0d8070e9c92c3
Brian Olson <brian.olson@gmail.com>
2020-08-05 19:51:49 +0000

    test asset balances; note indexes to speed things up

--- misc/speedtest.py
+++ misc/speedtest.py
@@ -172,6 +172,9 @@ def getAssets(rooturl):
 # /v2/assets/{asset-id}/transactions
 # equivalent to
 # /v2/transactions?asset-id=N
+#
+# To make fast:
+# CREATE INDEX CONCURRENTLY IF NOT EXISTS txn_asset ON txn (asset, round, intra);
 def assetTxns(rooturl, assets, n=1000, minTime=None, maxTime=10, ntxns=1000):
     rootparts = urllib.parse.urlparse(rooturl)
     rawurl = list(rootparts)
@@ -204,15 +207,53 @@ def assetTxns(rooturl, assets, n=1000, minTime=None, maxTime=10, ntxns=1000):
             break
         if maxTime and (dt > maxTime):
             break
-    logger.info('account recent txns: %d assets in %0.2f seconds, %.1f /s; %d txns, %.1f txn/s', count, dt, count/dt, txcount, txcount/dt)
+    logger.info('asset txns: %d assets in %0.2f seconds, %.1f /s; %d txns, %.1f txn/s', count, dt, count/dt, txcount, txcount/dt)
     return {'url':reporturl, 'assets': count, 'seconds': dt, 'txns': txcount}
 
+# /v2/assets/{asset-id}/balances -- maybe add index to account_asset table?
+#
+# To make fast:
+# CREATE INDEX CONCURRENTLY IF NOT EXISTS account_asset_asset ON account_asset (assetid, addr ASC);
+def assetBalances(rooturl, assets, n=1000, minTime=None, maxTime=10, ntxns=1000):
+    rootparts = urllib.parse.urlparse(rooturl)
+    rawurl = list(rootparts)
+    atxnsurl = list(rootparts)
+    assets = list(assets.keys())
+    random.shuffle(assets)
+    count = 0
+    balcount = 0
+    start = time.time()
+    reporturl = None
+    for assetid in assets:
+        atxnsurl[2] = os.path.join(rawurl[2], 'v2', 'assets', str(assetid), 'balances')
+        query = {'limit':ntxns}
+        atxnsurl[4] = urllib.parse.urlencode(query)
+        pageurl = urllib.parse.urlunparse(atxnsurl)
+        if reporturl is None:
+            reporturl = pageurl
+        logger.debug('GET %s', pageurl)
+        response = urllib.request.urlopen(pageurl)
+        if (response.code != 200) or not response.getheader('Content-Type').startswith('application/json'):
+            raise Exception("bad response to {!r}: {}".format(pageurl, response.reason))
+        ob = json.loads(response.read())
+        bals = ob.get('balances', [])
+        balcount += len(bals)
+        count += 1
+        dt = time.time() - start
+        if (n and (count >= n)) and (not minTime or (dt > minTime)):
+            break
+        if not n and minTime and (dt > minTime):
+            break
+        if maxTime and (dt > maxTime):
+            break
+    logger.info('asset balances: %d assets in %0.2f seconds, %.1f /s; %d bals, %.1f bal/s', count, dt, count/dt, balcount, balcount/dt)
+    return {'url':reporturl, 'assets': count, 'seconds': dt, 'balances': balcount}
+
 # TODO:
-# /v2/applications
-# /v2/applications/N
-# /v2/assets/N
-# /v2/assets/{asset-id}/balances
-# /v2/blocks/{round-number}
+# /v2/applications -- this should be fast because it's a single table scan
+# /v2/applications/N -- single row lookup, easy
+# /v2/assets/{asset-id} -- single row lookup, easy
+# /v2/blocks/{round-number} -- single row lookup, easy
 # /v2/transactions what search opts?
 
 
@@ -248,6 +289,8 @@ def main():
         assets, rept = getAssets(indexerurl)
         report.append(rept)
         rept = assetTxns(indexerurl, assets)
+        report.append(rept)
+        rept = assetBalances(indexerurl, assets)
         report.append(rept)
     json.dump(report, sys.stdout)
     sys.stdout.write('\n')
