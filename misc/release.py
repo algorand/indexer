5b3673b15740624d72c6a04061f8f60f5e4cb12f
Brian Olson <brianolson@users.noreply.github.com>
2020-09-21 16:47:20 -0400

    add version strings to release.py (#192)
    add version strings to release.py
    improve `algorand-indexer -v`, e.g.
    2.1.1-dev.unknown compiled at 2020-09-02T20:46:00+0000 from git hash 283e9ecc2f6c0a29d6b9d99b7f9bf3a99f7f52ad (modified)

--- misc/release.py
+++ misc/release.py
@@ -6,9 +6,12 @@
 # ve3/bin/python misc/release.py
 
 import argparse
+import base64
 import io
 import logging
 import os
+import re
+import shlex
 import subprocess
 import tarfile
 import time
@@ -96,14 +99,54 @@ def link(sourcepath, destpath):
         os.remove(destpath)
     os.link(sourcepath, destpath)
 
-def compile(goos=None, goarch=None):
+_tagpat = re.compile(r'tag:\s+([^,]+)')
+
+def compile_version_opts(release_version=None, allow_mismatch=False):
+    result = subprocess.run(['git', 'log', '-n', '1', '--pretty=%H %D'], stdout=subprocess.PIPE)
+    result.check_returncode()
+    so = result.stdout.decode()
+    githash, desc = so.split(None, 1)
+    tags = []
+    tag = None
+    for m in _tagpat.finditer(desc):
+        tag = m.group(1)
+        tags.append(tag)
+        if tag == release_version:
+            break
+    if tag != release_version:
+        if not allow_mismatch:
+            raise Exception('.version is {!r} but tags {!r}'.format(release_version, tags))
+        else:
+            logger.warning('.version is %r but tags %r', release_version, tags)
+    now = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime()) + '+0000'
+    result = subprocess.run(['git', 'status', '--porcelain'], stdout=subprocess.PIPE)
+    result.check_returncode()
+    if len(result.stdout) > 2:
+        dirty = "true"
+    else:
+        dirty = ""
+    # Note: keep these in sync with Makefile
+    ldflags = '-ldflags=-X github.com/algorand/indexer/version.Hash={}'.format(githash)
+    ldflags += ' -X github.com/algorand/indexer/version.Dirty={}'.format(dirty)
+    ldflags += ' -X github.com/algorand/indexer/version.CompileTime={}'.format(now)
+    ldflags += ' -X github.com/algorand/indexer/version.GitDecorateBase64={}'.format(base64.b64encode(desc.encode()).decode())
+    if release_version:
+        ldflags += ' -X github.com/algorand/indexer/version.ReleaseVersion={}'.format(release_version)
+    logger.debug('Hash=%r Dirty=%r CompileTime=%r decorate=%r ReleaseVersion=%r', githash, dirty, now, desc, release_version)
+    logger.debug('%s', ldflags)
+    return ldflags
+
+def compile(goos=None, goarch=None, ldflags=None):
     env = dict(os.environ)
     env['CGO_ENABLED'] = '0'
     if goos is not None:
         env['GOOS'] = goos
     if goarch is not None:
         env['GOARCH'] = goarch
-    subprocess.run(['go', 'build'], cwd='cmd/algorand-indexer', env=env).check_returncode()
+    cmd = ['go', 'build']
+    if ldflags is not None:
+        cmd.append(ldflags)
+    subprocess.run(cmd, cwd='cmd/algorand-indexer', env=env).check_returncode()
 
 def build_deb(debarch, version, outdir):
     os.makedirs('.deb_tmp/DEBIAN', exist_ok=True)
@@ -171,22 +214,51 @@ def build_tar(goos, goarch, version, outdir):
     tf.close()
     return tarname
 
+def hostOsArch():
+    result = subprocess.run(['go', 'env'], stdout=subprocess.PIPE)
+    result.check_returncode()
+    goenv = {}
+    for line in result.stdout.decode().splitlines():
+        line = line.strip()
+        k,v = line.split('=', 1)
+        goenv[k] = shlex.split(v)[0]
+    return goenv['GOHOSTOS'], goenv['GOHOSTARCH']
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--outdir", help="The output directory for the build assets", type=str, default=".")
-    args = parser.parse_args()
+    start = time.time()
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-o', '--outdir', help='The output directory for the build assets', type=str, default='.')
+    ap.add_argument('--no-deb', action='store_true', default=False, help='disable debian package building')
+    ap.add_argument('--host-only', action='store_true', default=False, help='only build for host OS and CPU')
+    ap.add_argument('--build-only', action='store_true', default=False, help="don't make tar or deb release")
+    ap.add_argument('--fake-release', action='store_true', default=False, help='relax some checks during release script development')
+    ap.add_argument('--verbose', action='store_true', default=False)
+    args = ap.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
     outdir = args.outdir
 
-    start = time.time()
-    logging.basicConfig(level=logging.INFO)
+    if args.host_only:
+        hostos, hostarch = hostOsArch()
+        logger.info('will only run %s %s', hostos, hostarch)
     with open('.version') as fin:
         version = fin.read().strip()
+    ldflags = compile_version_opts(version, allow_mismatch=args.fake_release)
     for goos, goarch, debarch in osArchArch:
+        if args.host_only and (goos != hostos or goarch != hostarch):
+            logger.debug('skip %s %s', goos, goarch)
+            continue
         logger.info('GOOS=%s GOARCH=%s DEB_HOST_ARCH=%s', goos, goarch, debarch)
-        compile(goos, goarch)
+        compile(goos, goarch, ldflags)
+        if args.build_only:
+            logger.debug('skip packaging')
+            continue
         tarname = build_tar(goos, goarch, version, outdir)
         logger.info('\t%s', tarname)
-        if debarch is not None:
+        if (not args.no_deb) and (debarch is not None):
             debname = build_deb(debarch, version, outdir)
             logger.info('\t%s', debname)
     dt = time.time() - start
@@ -195,4 +267,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
