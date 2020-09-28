15aba17178c814b80acc05ec8e570ac1f54ec930
Will Winder <wwinder.unh@gmail.com>
2020-09-28 15:27:31 -0400

    Fix comments.

--- api/middlewares/migration_middleware.go
+++ api/middlewares/migration_middleware.go
@@ -1,17 +1,22 @@
 package middlewares
 
 import (
-	"github.com/algorand/indexer/idb"
-	"github.com/labstack/echo/v4"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/algorand/indexer/idb"
 )
 
+// InProgressError is the error returned when a migration is in progress.
 var InProgressError = "Indexer migration in progress, please wait."
+
+// MigrationMiddleware makes sure a 500 error is returned when the IndexerDb has a migration in progress.
 type MigrationMiddleware struct {
 	idb       idb.IndexerDb
 }
 
-// MakeAuth constructs the auth middleware function
+// MakeMigrationMiddleware constructs the migration middleware
 func MakeMigrationMiddleware(idb idb.IndexerDb) echo.MiddlewareFunc {
 	mw := MigrationMiddleware{
 		idb:       idb,
@@ -20,8 +25,7 @@ func MakeMigrationMiddleware(idb idb.IndexerDb) echo.MiddlewareFunc {
 	return mw.handler
 }
 
-// Auth takes a logger and an array of api token and return a middleware function
-// that ensures one of the api tokens was provided.
+// handler returns a 500 if the IndexerDb is migrating.
 func (mm *MigrationMiddleware) handler(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		h, err := mm.idb.Health()
--- api/middlewares/migration_middleware_test.go
+++ api/middlewares/migration_middleware_test.go
@@ -2,23 +2,24 @@ package middlewares
 
 import (
 	"errors"
-	"github.com/algorand/indexer/idb"
-	"github.com/algorand/indexer/idb/mocks"
+	"testing"
+
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
-	"testing"
+
+	"github.com/algorand/indexer/idb"
+	"github.com/algorand/indexer/idb/mocks"
 )
 
 var errSuccess = errors.New("unexpected success")
 var e = echo.New()
 var testAPIHeader = "API-Header-Whatever"
 
-// success is the "next" handler, it is only called when auth allows the request to continue
+// success is the "next" handler, it is only called when a request is allowed to continue
 func success(ctx echo.Context) error {
 	return errSuccess
 }
-
 
 func TestMigrationMiddlewareWaiting(t *testing.T) {
 	mockIndexer := &mocks.IndexerDb{}
@@ -31,7 +32,7 @@ func TestMigrationMiddlewareWaiting(t *testing.T) {
 	handler := MakeMigrationMiddleware(mockIndexer)(success)
 	err := handler(e.NewContext(nil, nil))
 
-	require.Error(t, err, InProgressError)
+	require.Error(t, err, InProgressError, `'IsMigrating' is true, so we should see an InProgressError`)
 }
 
 func TestMigrationMiddlewareDone(t *testing.T) {
@@ -45,5 +46,5 @@ func TestMigrationMiddlewareDone(t *testing.T) {
 	handler := MakeMigrationMiddleware(mockIndexer)(success)
 	err := handler(e.NewContext(nil, nil))
 
-	require.Error(t, err, errSuccess.Error())
+	require.Error(t, err, errSuccess.Error(), `'IsMigrating' is false, so errSuccess should pass through`)
 }
