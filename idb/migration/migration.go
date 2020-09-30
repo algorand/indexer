bd6be79714bc00645e0776decf916788c8fcc17c
Will Winder <wwinder.unh@gmail.com>
2020-09-30 11:23:24 -0400

    More PR feedback and minor fixes

--- idb/migration/migration.go
+++ idb/migration/migration.go
@@ -17,6 +17,7 @@ const StatusErrorPrefix = "error during migration "
 // Handler is the function which will be executed to perform the migration for this task.
 type Handler func() error
 
+// Task is used to define a migration.
 type Task struct {
 	// MigrationId is an internal ID that can be used by IndexerDb implementations.
 	MigrationId int
@@ -33,18 +34,27 @@ type Task struct {
 	Description string
 }
 
+// State is the current status of the migration.
 type State struct {
+	// Err is the last error which occurred during the migration. On an error the migration should halt.
 	Err      error
+
+	// Status is the most recent status message.
 	Status   string
+
+	// Running indicates whether or not a migration is currently running.
 	Running  bool
+
+	// Blocking indicates that one or more tasks have requested that the DB remain unavailable until they complete.
 	Blocking bool
 }
 
+// Migration manages the execution of multiple migration tasks and provides a mechanism for concurrent status checks.
 type Migration struct {
-	State
 	mutex      sync.RWMutex
 	tasks      []Task
 	blockUntil int
+	state      State
 }
 
 // Broken out to allow for testing.
@@ -77,10 +87,11 @@ func (m *Migration) setTasks(migrationTasks []Task) error {
 	return nil
 }
 
+// MakeMigration initializes
 func MakeMigration(migrationTasks []Task) (*Migration, error) {
 	m := &Migration{
 		tasks: migrationTasks,
-		State: State{
+		state: State{
 			Err:      nil,
 			Status:   StatusPending,
 			Running:  false,
@@ -92,40 +103,45 @@ func MakeMigration(migrationTasks []Task) (*Migration, error) {
 	return m, err
 }
 
+// GetStatus returns the current status of the migration. This function is thread safe.
 func (m *Migration) GetStatus() State {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
 
 	return State{
-		Err:      m.Err,
-		Status:   m.Status,
-		Running:  m.Running,
-		Blocking: m.Blocking,
+		Err:      m.state.Err,
+		Status:   m.state.Status,
+		Running:  m.state.Running,
+		Blocking: m.state.Blocking,
 	}
 }
 
+// update is a helper to set values in a thread safe way.
 func (m *Migration) update(err error, status string, running bool, blocking bool) {
 	m.mutex.Lock()
 
 	defer m.mutex.Unlock()
 
-	if err != m.Err {
-		m.Err = err
+	if err != m.state.Err {
+		m.state.Err = err
 	}
 
-	if status != m.Status {
-		m.Status = status
+	if status != m.state.Status {
+		m.state.Status = status
 	}
 
-	if running != m.Running {
-		m.Running = running
+	if running != m.state.Running {
+		m.state.Running = running
 	}
 
-	if blocking != m.Blocking {
-		m.Blocking = blocking
+	if blocking != m.state.Blocking {
+		m.state.Blocking = blocking
 	}
 }
 
+// RunMigrations runs all tasks which have been loaded into the migration. It will update the status accordingly as the
+// migration runs. This call will block execution until it completes and should be run in a go routine if that is not
+// expected.
 func (m *Migration) RunMigrations() {
 	blocking := true
 	for _, task := range m.tasks {
@@ -138,7 +154,7 @@ func (m *Migration) RunMigrations() {
 
 		if err != nil {
 			err := fmt.Errorf("%s%d (%s): %v", StatusErrorPrefix, task.MigrationId, task.Description, err)
-			// TODO: If a migration failed, should we block queries?
+			// If a migration failed, mark that the migration is blocking and terminate.
 			blocking = true
 			m.update(err, err.Error(), false, blocking)
 			return
