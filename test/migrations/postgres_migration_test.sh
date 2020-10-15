d715cd400d7a8b3bd975c590bc6cd7e2cd94d2e9
Will Winder <wwinder.unh@gmail.com>
2020-10-15 10:54:59 -0400

    Add some documentation to the new script.

--- test/migrations/postgres_migration_test.sh
+++ test/migrations/postgres_migration_test.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+
+# Tests migrations by hooking up indexer to a preconfigured postres database.
+#
+# Postgres is run inside a docker container, and initialized with a dump file
+# created with pg_dump.
+#
+# Creating the dumpfile is outside the scope of this script. The SDK test
+# environment can be used for this. Make sure that the version of indexer being
+# used does not include the new migration. Once started submit transactions
+# that will allow the migration to be tested. When finished use "pg_dump" to
+# save the postgres state.
+#
+# Copy the postgres dump file into this directory and write some tests to make
+# sure the migration works.
+
 set -e
 
 # This script only works when CWD is 'test/migrations'
@@ -37,7 +52,7 @@ function wait_for_ready() {
   until [ "$n" -ge 20 ]
   do
     curl -s -q "$NET/health" | grep '"migration-status":"Migrations Complete"' > /dev/null 2>&1 && break
-    n=$((n+1)) 
+    n=$((n+1))
     sleep 1
   done
   set -e
