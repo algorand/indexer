4b2a49937b759eed6fd63abd06957ef4048d658f
Will Winder <wwinder.unh@gmail.com>
2021-02-09 14:57:34 -0500

    Alternate validate_accounting script.

---
+++ misc/validate_accounting.sh
@@ -0,0 +1,307 @@
+#!/usr/bin/env bash
+#
+# This script requires the convertToAddress.go tool to be built and provided
+# with the --convert_addr field
+
+function help () {
+  echo "This script compares account data in Indexer to data in algod."
+  echo ""
+  echo "Requires 'psql' command to be available."
+  echo ""
+  echo "options:"
+  echo "  --convert_addr  -> Path to the convert_addr utility."
+  echo "  --pg_user       -> Postgres username."
+  echo "  --pg_pass       -> Postgres password."
+  echo "  --pg_url        -> Postgres url (without http)."
+  echo "  --pg_port       -> Postgres port."
+  echo "  --pg_db         -> Postgres database."
+  echo "  --datadir       -> Full path to data directory."
+  echo "  --algod         -> Algod url (with http), required if datadir is not available."
+  echo "  --algod_token   -> Algod API token, required if datadir is not available."
+  echo "  --indexer       -> Indexer url (with http)."
+  echo "  --indexer_token -> Indexer API token. If not used, set to any non empty value."
+}
+
+
+LIMIT=200000
+
+# All accounts
+#SELECTION_QUERY="select encode(addr,'base64') from account"
+SELECTION_QUERY="select encode(addr,'base64') from account where deleted is not null"
+# Only big accounts
+#SELECTION_QUERY="select encode(addr,'base64') from account WHERE microalgos > 100000000000 limit $LIMIT"
+# Only check accounts with rewards
+#SELECTION_QUERY="select encode(addr,'base64'), rewards_total, microalgos from account where rewards_total > 0 AND created_at != 0 limit $LIMIT"
+# Check all accounts
+#SELECTION_QUERY="select encode(addr,'base64'), rewards_total from account"
+# If run during a migration this query allow accounts with partial rewards to slip in (and they should fail)
+#SELECTION_QUERY="select encode(addr,'base64'), rewards_total from account where rewards_total > 0 limit $LIMIT"
+
+SINGLE_ACCT_QUERY="select rewards_total from account where addr=decode('B64', 'base64')"
+
+START_TIME=$SECONDS
+PGUSER=
+PGPASSWORD=
+PGHOST=
+PGPORT=
+PGDB=
+ALGOD_TOKEN=
+ALGOD_NET=
+INDEXER_NET=
+INDEXER_TOKEN=""
+TEST=
+CONVERT_ADDR=
+
+while (( "$#" )); do
+  case "$1" in
+    --convert_addr)
+      shift
+      CONVERT_ADDR="$1"
+      ;;
+    --pg_user)
+      shift
+      PGUSER="$1"
+      ;;
+    --pg_pass)
+      shift
+      PGPASSWORD="$1"
+      ;;
+    --pg_url)
+      shift
+      PGHOST="$1"
+      ;;
+    --pg_port)
+      shift
+      PGPORT="$1"
+      ;;
+    --pg_db)
+      shift
+      PGDB="$1"
+      ;;
+    --indexer)
+      shift
+      INDEXER_NET="$1"
+      ;;
+    --indexer_token)
+      shift
+      INDEXER_TOKEN="$1"
+      ;;
+    --algod)
+      shift
+      ALGOD_NET="$1"
+      ;;
+    --algod_token)
+      shift
+      ALGOD_TOKEN="$1"
+      ;;
+    --datadir)
+      shift
+      ALGOD_TOKEN="$(cat $1/algod.token)"
+      ALGOD_NET="$(cat $1/algod.net)"
+      ;;
+    --test)
+      TEST=1
+      ;;
+    -h|--help)
+      help
+      exit
+      ;;
+  esac
+  shift
+done
+
+if [ -z $CONVERT_ADDR ] || [ -z $PGUSER ] || [ -z $PGPASSWORD ] || [ -z $PGPORT ] || [ -z $PGHOST ] || [ -z $PGDB ] || [ -z $INDEXER_NET ] || [ -z $INDEXER_TOKEN ] || [ -z $ALGOD_NET ] || [ -z $ALGOD_TOKEN ]; then
+  help
+  exit
+fi
+
+# Required for psql
+export PGPASSWORD
+
+# $1 - the query to execute
+# $2 - if not empty prints the command before executing it
+function psql_query {
+  if [ ! -z $2 ]; then
+    echo "psql -t -h $PGHOST -d $PGDB -XA -p $PGPORT -U $PGUSER -c \"$1\""
+  fi
+  psql -t -h $PGHOST -d $PGDB -XA -p $PGPORT -U $PGUSER -c "$1"
+}
+
+function stats {
+  ELAPSED=$(($SECONDS - $START_TIME))
+
+  echo ""
+  echo ""
+  echo "Number of errors: [$ERROR_COUNT / $ACCOUNT_COUNT]"
+  echo "Retry count: $RETRY_COUNT"
+  printf 'Test run duration: %02dh:%02dm:%02ds\n' $(($ELAPSED/3600)) $(($ELAPSED%3600/60)) $(($ELAPSED%60))
+}
+
+function print_details {
+  printf "\n%-${GUTTER}s : Accounting mismatch for %s\n" "($ERROR_COUNT)" "$ACCT"
+  printf "\nIndexer JSON:\n\n$INDEXER_ACCT_NORMALIZED"
+  printf "\nALGOD JSON:\n\n$ALGOD_ACCT_NORMALIZED"
+  #printf "\n| Source          | %-15s | %-15s | %-15s | %-15s |\n" "balance" "distributed" "pending" "total"
+  #printf "| --------------- | --------------- | --------------- | --------------- | --------------- |\n"
+  #printf "| %-15s | %-15s | %-15s | %-15s | %-15s |\n" "Indexer" "$INDEXER_BALANCE" "$INDEXER_REWARDS_DIST" $INDEXER_REWARDS_PENDING "$INDEXER_REWARDS*"
+  #printf "| %-15s | %-15s | %-15s | %-15s | %-15s |\n" "Algod" "$ALGOD_BALANCE" "$ALGOD_REWARDS_DIST*" $ALGOD_REWARDS_PENDING "$ALGOD_REWARDS"
+}
+
+# Fancy jq function to normalize the json coming out of indexer or algod.
+# $1 - json data
+function normalize_json {
+  echo "$1" | jq -M\
+    '
+    # If there is a top level account field (indexer), use it.
+    if .account then .account else . end
+      |
+    # Remove uninitialized accounts returned by algod
+    select(.amount != 0)
+      |
+    # Remove any object marked deleted in indexer
+    walk (
+      if type == "object" then
+        del( . | select(."deleted" == true))
+      else
+        .
+      end)
+      |
+    # Remove fields which are not universally supported
+    walk(
+        if type == "object" then
+          with_entries(
+            select(
+              # deleted is only in indexer
+              .key != "deleted" and
+
+              # at-round fields are only supported in indexer
+              .key != "created-at-round" and
+              .key != "deleted-at-round" and
+              .key != "destroyed-at-round" and
+              .key != "oopted-in-at-roundptin-at-round" and
+              .key != "opted-in-at-round" and
+              .key != "opted-out-at-round" and
+              .key != "closeout-at-round" and
+              .key != "closed-out-at-round" and
+              .key != "closed-at-round" and
+
+              # indexer does not attach creator to asset holdings
+              .key != "creator" and
+
+              # indexer puts a special sig-type field at the top level
+              .key != "sig-type" and
+
+              # indexer and algod are almost always off by 1 round
+              .key != "round" and
+
+              # algod seems to look this up on demand, indexer has a stale value
+              .key != "reward-base" and
+
+              # make sure empty fields are handled consistently
+              .value != null and
+              .value != "" and
+              .value != [] and
+              .value != {} and
+              .value != 0)
+                |
+              # Indexer adds a space to NotParticipating
+              if .key == "status" and .value == "Not Participating" then
+                .value = "NotParticipating"
+              else 
+                .
+              end
+            )
+        elif type == "array" then
+          map(select(. != null and . != {} and . != []))
+        else
+          .
+        end)'
+}
+
+# $1 - account address
+function update_account {
+  #echo "curl -s -q \"$INDEXER_NET/v2/accounts/$1?pretty\" -H \"Authorization: Bearer $INDEXER_TOKEN\""
+  INDEXER_ACCT=$(curl -s -q "$INDEXER_NET/v2/accounts/$1?pretty" -H "Authorization: Bearer $INDEXER_TOKEN")
+  INDEXER_ACCT_NORMALIZED=$(normalize_json "$INDEXER_ACCT")
+  INDEXER_BALANCE=$(echo ${INDEXER_ACCT} | jq '."account"."amount"')
+  #INDEXER_REWARDS_DIST=$(psql_query "${SINGLE_ACCT_QUERY/B64/$ACCTB64}")
+  #INDEXER_REWARDS_PENDING=$(echo ${INDEXER_ACCT} | jq '."account"."pending-rewards"')
+  #INDEXER_REWARDS=$((INDEXER_REWARDS_DIST+INDEXER_REWARDS_PENDING))
+
+  #echo "curl -s -q -H \"Authorization: Bearer $ALGOD_TOKEN\" \"$ALGOD_NET/v2/accounts/$1?pretty\""
+  ALGOD_ACCT=$(curl -s -q -H "Authorization: Bearer $ALGOD_TOKEN" "$ALGOD_NET/v2/accounts/$1?pretty")
+  ALGOD_ACCT_NORMALIZED=$(normalize_json "$ALGOD_ACCT")
+  ALGOD_BALANCE=$(echo ${ALGOD_ACCT} | jq '."amount"')
+  #ALGOD_REWARDS=$(echo "$ALGOD_ACCT" | jq '.rewards')
+  #ALGOD_REWARDS_PENDING=$(echo "$ALGOD_ACCT" | jq '."pending-rewards"')
+  #ALGOD_REWARDS_DIST=$((ALGOD_REWARDS-ALGOD_REWARDS_PENDING))
+
+
+  #echo -e "\nINDEXER\n$INDEXER_ACCT"
+  #echo -e "\nALGOD\n$ALGOD_ACCT"
+}
+
+#####################
+# Start the script! #
+#####################
+
+# Make sure stats are reported after ctrl-C, this script will probably never end.
+trap stats EXIT
+
+# Print connection tests if enabled.
+if [ ! -z $TEST ]; then
+  echo "psql configuration test:"
+  psql_query "select * from metastate" 1
+
+  echo -e "\nindexer configuration test:"
+  echo -e "~$ "'curl -s -q "$INDEXER_NET/health?pretty" -H "Authorization: Bearer $INDEXER_TOKEN"'
+  curl -s -q "$INDEXER_NET/health?pretty" -H "Authorization: Bearer $INDEXER_TOKEN"
+  echo -e "\nalgod configuration test:"
+  echo -e "~$"' curl -s -q "$ALGOD_NET/v2/status?pretty" -H "Authorization: Bearer $ALGOD_TOKEN"'
+  curl -s -q "$ALGOD_NET/v2/status?pretty" -H "Authorization: Bearer $ALGOD_TOKEN"
+  echo ""
+fi
+
+ACCOUNT_COUNT=0
+ERROR_COUNT=0
+RETRY_COUNT=0
+GUTTER=8 # printf formatting
+
+# Loop through all accounts in SELECTION_QUERY
+while read -r line; do
+  if [ $(($ACCOUNT_COUNT%50)) -eq 0 ]; then
+      printf "\n%-${GUTTER}d : " "$ACCOUNT_COUNT"
+  fi
+  
+  ACCT=$($CONVERT_ADDR -addr $line)
+
+  # get normalized account details.
+  # busy accounts desynchronize regularly as indexer lags behind slightly, so validate in a loop.
+  n=0
+  until [ "$n" -ge 3 ]; do
+    update_account $ACCT
+    # break out on succes 
+    [ "$INDEXER_ACCT_NORMALIZED" == "$ALGOD_ACCT_NORMALIZED" ] && break
+    ((n++))
+    sleep 1
+  done
+
+  if [ "$n" -ne 0 ]; then
+    ((RETRY_COUNT++))
+  fi
+
+  if [ "$INDEXER_ACCT_NORMALIZED" != "$ALGOD_ACCT_NORMALIZED" ] ; then
+    ((ERROR_COUNT++))
+    print_details
+    PAD=$(($ACCOUNT_COUNT%50+11))
+    printf "%*s" $PAD
+  fi
+  ((ACCOUNT_COUNT++))
+
+  if [ "$n" -ne 0 ]; then
+    printf "$n"
+  else
+    printf "."
+  fi
+done < <((psql_query "$SELECTION_QUERY")) # TODO: pass accounts in with a file?
+
