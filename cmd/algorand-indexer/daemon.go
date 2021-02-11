43658a5142ba1d69833bdf382b8d136ab9870687
Will Winder <wwinder.unh@gmail.com>
2021-02-11 10:38:27 -0500

    We no longer need to adjust this.

--- cmd/algorand-indexer/daemon.go
+++ cmd/algorand-indexer/daemon.go
@@ -82,7 +82,7 @@ var daemonCmd = &cobra.Command{
 			maxRound, err := db.GetMaxRound()
 			maybeFail(err, "failed to get max round, %v", err)
 			if maxRound != 0 {
-				bot.SetNextRound(maxRound + 1)
+				bot.SetNextRound(maxRound)
 			}
 			bih := blockImporterHandler{
 				imp:   importer.NewDBImporter(db),
