030b0963c2a4bc9ce1804ea26e78b00b52fadcac
Will Winder <wwinder.unh@gmail.com>
2021-06-11 16:46:23 -0400

    Lock down scope even further.

--- cmd/block-generator/daemon.go
+++ cmd/block-generator/daemon.go
@@ -10,27 +10,28 @@ import (
 	"github.com/algorand/indexer/cmd/block-generator/generator"
 )
 
-var configFile string
-var port uint64
-
 const configFileName = "block_generator_config"
 
 func init() {
 	rand.Seed(12345)
 
+	var configFile string
+	var port uint64
+
+	var daemonCmd = &cobra.Command{
+		Use:   "daemon",
+		Short: "Start the generator daemon in standalone mode.",
+		Run: func(cmd *cobra.Command, args []string) {
+			generator.StartServer(configFile, port)
+		},
+	}
+
 	daemonCmd.Flags().StringVarP(&configFile, "config", "c", configFileName, fmt.Sprintf("Override default config file from '%s'.", configFileName))
 	daemonCmd.Flags().Uint64VarP(&port, "port", "p", 4010, "Port to start the server at.")
 
-	rootCmd.AddCommand(daemonCmd)
-
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
+
+	rootCmd.AddCommand(daemonCmd)
 }
 
-var daemonCmd = &cobra.Command{
-	Use:   "daemon",
-	Short: "Start the generator daemon in standalone mode.",
-	Run: func(cmd *cobra.Command, args []string) {
-		generator.StartServer(configFile, port)
-	},
-}
