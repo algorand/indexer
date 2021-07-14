6cb12711fe683b3604dac5cf9b127282919a8f37
Brian Olson <brian.olson@gmail.com>
2021-07-14 14:07:59 -0400

    expand test of emoji. fix

--- idb/postgres/internal/encoding/encode_test.go
+++ idb/postgres/internal/encoding/encode_test.go
@@ -1,12 +1,16 @@
 package encoding
 
 import (
+	"encoding/base64"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestEscapeNulls(t *testing.T) {
+	const moreEmojiThanEmacsCanHandleBase64 = "8J+YgPCfporwn42M8J+lj/Cfmo7wn5ec8J+VifCfj7TigI3imKDvuI8="
+	manyEmojiBytes, _ := base64.StdEncoding.DecodeString(moreEmojiThanEmacsCanHandleBase64)
+	manyEmoji := string(manyEmojiBytes)
 	tests := []struct {
 		input    string
 		expected string
@@ -15,6 +19,8 @@ func TestEscapeNulls(t *testing.T) {
 		{"ao\x00eu", "ao\\u0000eu"},      // zero byte
 		{"ao\\u0000eu", "ao\\\\u0000eu"}, // \ -> \\
 		{"ao\xc0 eu", "ao\\u00c0 eu"},    // invalid utf8 \xc0\x20
+		{"ăѣ𝔠ծềſģȟᎥ𝒋ǩľḿꞑȯ𝘱𝑞𝗋𝘴ȶ𝞄𝜈ψ𝒙𝘆𝚣1234567890!@#$%^&*()-_=+[{]};:'\",<.>/?", "ăѣ𝔠ծềſģȟᎥ𝒋ǩľḿꞑȯ𝘱𝑞𝗋𝘴ȶ𝞄𝜈ψ𝒙𝘆𝚣1234567890!@#$%^&*()-_=+[{]};:'\",<.>/?"},
+		{manyEmoji, manyEmoji},
 	}
 
 	for _, tc := range tests {
