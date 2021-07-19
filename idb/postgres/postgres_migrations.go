1c14ff214367a83d97bd56c0e4f88e95f55a03c1
Tolik Zinovyev <tolik@algorand.com>
2021-07-19 11:54:30 -0400

    Delete other released migrations. (#574)

--- idb/postgres/postgres_migrations.go
+++ idb/postgres/postgres_migrations.go
@@ -8,8 +8,6 @@ import (
 	"database/sql"
 	"fmt"
 
-	sdk_types "github.com/algorand/go-algorand-sdk/types"
-
 	"github.com/algorand/indexer/idb"
 	"github.com/algorand/indexer/idb/migration"
 	"github.com/algorand/indexer/idb/postgres/internal/encoding"
@@ -50,13 +48,9 @@ func init() {
 type MigrationState struct {
 	NextMigration int `json:"next"`
 
-	// NextRound used for m0,m9 to checkpoint progress.
-	NextRound int64 `json:"round,omitempty"`
-
-	// NextAssetID used for m3 to checkpoint progress.
-	NextAssetID int64 `json:"assetid,omitempty"`
-
-	// The following two are used for m7 to save progress.
+	// The following are deprecated.
+	NextRound    int64  `json:"round,omitempty"`
+	NextAssetID  int64  `json:"assetid,omitempty"`
 	PointerRound *int64 `json:"pointerRound,omitempty"`
 	PointerIntra *int64 `json:"pointerIntra,omitempty"`
 
@@ -103,12 +97,8 @@ func needsMigration(state MigrationState) bool {
 // upsertMigrationState updates the migration state, and optionally increments
 // the next counter with an existing transaction.
 // If `tx` is nil, use a normal query.
-func upsertMigrationState(db *IndexerDb, tx *sql.Tx, state *MigrationState, incrementNextMigration bool) error {
-	if incrementNextMigration {
-		state.NextMigration++
-	}
+func upsertMigrationState(db *IndexerDb, tx *sql.Tx, state *MigrationState) error {
 	migrationStateJSON := encoding.EncodeJSON(state)
-
 	return db.setMetastate(tx, migrationMetastateKey, string(migrationStateJSON))
 }
 
@@ -265,269 +255,20 @@ func m11AssetHoldingFrozen(db *IndexerDb, state *MigrationState) error {
 	return fmt.Errorf(unsupportedMigrationErrorMsg, "2.5.0")
 }
 
-// Reusable update batch function. Provide a query and an array of argument arrays to pass  to that query.
-func updateBatch(db *IndexerDb, updateQuery string, data [][]interface{}) error {
-	db.accountingLock.Lock()
-	defer db.accountingLock.Unlock()
-
-	tx, err := db.db.Begin()
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback() // ignored if .Commit() first
-
-	update, err := tx.Prepare(updateQuery)
-	if err != nil {
-		return fmt.Errorf("error preparing update query: %v", err)
-	}
-	defer update.Close()
-
-	for _, txpr := range data {
-		_, err = update.Exec(txpr...)
-		if err != nil {
-			return fmt.Errorf("problem updating row (%v): %v", txpr, err)
-		}
-	}
-
-	return tx.Commit()
-}
-
 // FixFreezeLookupMigration is a migration to add txn_participation entries for freeze address in freeze transactions.
 func FixFreezeLookupMigration(db *IndexerDb, state *MigrationState) error {
-	// Technically with this query no transactions are needed, and the accounting state doesn't need to be locked.
-	updateQuery := "INSERT INTO txn_participation (addr, round, intra) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING"
-	query := fmt.Sprintf("select decode(txn.txn->'txn'->>'fadd','base64'),round,intra from txn where typeenum = %d AND txn.txn->'txn'->'snd' != txn.txn->'txn'->'fadd'", idb.TypeEnumAssetFreeze)
-	rows, err := db.db.Query(query)
-	if err != nil {
-		return fmt.Errorf("unable to query transactions: %v", err)
-	}
-	defer rows.Close()
-
-	txprows := make([][]interface{}, 0)
-
-	// Loop through all transactions and compute account data.
-	db.log.Print("loop through all freeze transactions")
-	for rows.Next() {
-		var addr []byte
-		var round, intra uint64
-		err = rows.Scan(&addr, &round, &intra)
-		if err != nil {
-			return fmt.Errorf("error scanning row: %v", err)
-		}
-
-		txprows = append(txprows, []interface{}{addr, round, intra})
-
-		if len(txprows) > 5000 {
-			err = updateBatch(db, updateQuery, txprows)
-			if err != nil {
-				return fmt.Errorf("updating batch: %v", err)
-			}
-			txprows = txprows[:0]
-		}
-	}
-
-	if rows.Err() != nil {
-		return fmt.Errorf("error while processing freeze transactions: %v", rows.Err())
-	}
-
-	// Commit any leftovers
-	if len(txprows) > 0 {
-		err = updateBatch(db, updateQuery, txprows)
-		if err != nil {
-			return fmt.Errorf("updating batch: %v", err)
-		}
-	}
-
-	// Update migration state
-	return upsertMigrationState(db, nil, state, true)
-}
-
-type account struct {
-	address  sdk_types.Address
-	closedAt uint64 // the round when the account was last closed
-}
-
-func getAccounts(db *sql.DB) ([]account, error) {
-	query := "SELECT addr, closed_at FROM account WHERE closed_at IS NOT NULL AND deleted = false " +
-		"AND account_data IS NOT NULL"
-	rows, err := db.Query(query)
-	if err != nil {
-		return []account{}, err
-	}
-	defer rows.Close()
-
-	var res []account
-
-	for rows.Next() {
-		var addrBytes []byte
-		var closedAt sql.NullInt64
-
-		err = rows.Scan(&addrBytes, &closedAt)
-		if err != nil {
-			return []account{}, err
-		}
-
-		var addr sdk_types.Address
-		copy(addr[:], addrBytes)
-		res = append(res, account{addr, uint64(closedAt.Int64)})
-	}
-	if err := rows.Err(); err != nil {
-		return []account{}, err
-	}
-
-	return res, nil
-}
-
-func fixAuthAddr(db *IndexerDb, account account) error {
-	db.accountingLock.Lock()
-	defer db.accountingLock.Unlock()
-
-	f := func(ctx context.Context, tx *sql.Tx) error {
-		defer tx.Rollback()
-
-		rowsCh := make(chan idb.TxnRow)
-
-		// This will not work properly if the account was closed and reopened in the same round
-		// but that's unlikely to happen.
-		trueValue := true
-		tf := idb.TransactionFilter{
-			Address:     account.address[:],
-			AddressRole: idb.AddressRoleSender,
-			RekeyTo:     &trueValue,
-			MinRound:    account.closedAt,
-			Limit:       1,
-		}
-		go func() {
-			db.yieldTxns(ctx, tx, tf, rowsCh)
-			close(rowsCh)
-		}()
-
-		found := false
-		for txnRow := range rowsCh {
-			if txnRow.Error != nil {
-				return txnRow.Error
-			}
-			found = true
-		}
-
-		if found {
-			return nil
-		}
-
-		// No results. Delete the key.
-		db.log.Printf("clearing auth addr for account %s", account.address.String())
-
-		query := "UPDATE account SET account_data = account_data - 'spend' WHERE addr = $1"
-		_, err := tx.Exec(query, account.address[:])
-		if err != nil {
-			return err
-		}
-
-		return tx.Commit()
-	}
-
-	return db.txWithRetry(context.Background(), serializable, f)
-}
-
-func fixKeyreg(db *IndexerDb, account account) error {
-	db.accountingLock.Lock()
-	defer db.accountingLock.Unlock()
-
-	f := func(ctx context.Context, tx *sql.Tx) error {
-		defer tx.Rollback()
-
-		rowsCh := make(chan idb.TxnRow)
-
-		tf := idb.TransactionFilter{
-			Address:     account.address[:],
-			AddressRole: idb.AddressRoleSender,
-			MinRound:    account.closedAt,
-			TypeEnum:    idb.TypeEnumKeyreg,
-			Limit:       1,
-		}
-		go func() {
-			db.yieldTxns(ctx, tx, tf, rowsCh)
-			close(rowsCh)
-		}()
-
-		found := false
-		for txnRow := range rowsCh {
-			if txnRow.Error != nil {
-				return txnRow.Error
-			}
-			found = true
-		}
-
-		if found {
-			return nil
-		}
-
-		// No results. Delete keyreg fields.
-		db.log.Printf("clearing keyreg fields for account %s", account.address.String())
-
-		query := "UPDATE account SET account_data = account_data - 'vote' - 'sel' - 'onl' - " +
-			"'voteFst' - 'voteLst' - 'voteKD' WHERE addr = $1"
-		_, err := tx.Exec(query, account.address[:])
-		if err != nil {
-			return err
-		}
-
-		return tx.Commit()
-	}
-
-	return db.txWithRetry(context.Background(), serializable, f)
+	return fmt.Errorf(unsupportedMigrationErrorMsg, "2.5.0")
 }
 
 // ClearAccountDataMigration clears account data for accounts that have been closed.
 func ClearAccountDataMigration(db *IndexerDb, state *MigrationState) error {
-	// Clear account_data column for deleted accounts.
-	query := "UPDATE account SET account_data = NULL WHERE deleted = true;"
-	if _, err := db.db.Exec(query); err != nil {
-		return fmt.Errorf("error clearing deleted accounts: %v", err)
-	}
-
-	// Clear account data for some reopened accounts.
-	accounts, err := getAccounts(db.db)
-	if err != nil {
-		return fmt.Errorf("error getting accounts: %v", err)
-	}
-	db.log.Printf("checking %d accounts that are reopened and have account data", len(accounts))
-
-	for _, account := range accounts {
-		if err := fixAuthAddr(db, account); err != nil {
-			return fmt.Errorf("error clearing auth addr: %v", err)
-		}
-		if err := fixKeyreg(db, account); err != nil {
-			return fmt.Errorf("error clearing keyreg fields: %v", err)
-		}
-	}
-
-	state.NextMigration++
-	migrationStateJSON := encoding.EncodeJSON(state)
-	_, err = db.db.Exec(setMetastateUpsert, migrationMetastateKey, migrationStateJSON)
-	if err != nil {
-		return fmt.Errorf("metastate upsert error: %v", err)
-	}
-
-	return nil
+	return fmt.Errorf(unsupportedMigrationErrorMsg, "2.5.0")
 }
 
 // MakeDeletedNotNullMigration makes "deleted" columns NOT NULL in tables
 // account, account_asset, asset, app, account_app.
 func MakeDeletedNotNullMigration(db *IndexerDb, state *MigrationState) error {
-	queries := []string{
-		"UPDATE account SET deleted = false WHERE deleted is NULL",
-		"ALTER TABLE account ALTER COLUMN deleted SET NOT NULL",
-		"UPDATE account_asset SET deleted = false WHERE deleted is NULL",
-		"ALTER TABLE account_asset ALTER COLUMN deleted SET NOT NULL",
-		"UPDATE asset SET deleted = false WHERE deleted is NULL",
-		"ALTER TABLE asset ALTER COLUMN deleted SET NOT NULL",
-		"UPDATE app SET deleted = false WHERE deleted is NULL",
-		"ALTER TABLE app ALTER COLUMN deleted SET NOT NULL",
-		"UPDATE account_app SET deleted = false WHERE deleted is NULL",
-		"ALTER TABLE account_app ALTER COLUMN deleted SET NOT NULL",
-	}
-	return sqlMigration(db, state, queries)
+	return fmt.Errorf(unsupportedMigrationErrorMsg, "2.5.0")
 }
 
 // MaxRoundAccountedMigration converts the import state.
@@ -577,7 +318,7 @@ func MaxRoundAccountedMigration(db *IndexerDb, migrationState *MigrationState) e
 			}
 		}
 
-		err = upsertMigrationState(db, tx, &nextMigrationState, false)
+		err = upsertMigrationState(db, tx, &nextMigrationState)
 		if err != nil {
 			return err
 		}
