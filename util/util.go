1c50426da23e7433c77c254acad2f18028fdce1e
Will Winder <wwinder.unh@gmail.com>
2021-07-22 14:08:22 -0400

    Fix merge error.

--- util/util.go
+++ util/util.go
@@ -24,15 +24,6 @@ func PrintableUTF8OrEmpty(in string) string {
 	return in
 }
 
-// KeysStringInt returns all of the keys in the map joined by a comma.
-func KeysStringInt(m map[string]int) string {
-	keys := make([]string, 0, len(m))
-	for k := range m {
-		keys = append(keys, k)
-	}
-	return strings.Join(keys, ", ")
-}
-
 // KeysStringBool returns all of the keys in the map joined by a comma.
 func KeysStringBool(m map[string]bool) string {
 	keys := make([]string, 0, len(m))
