433c98e50e7584f217e120b2c5afb19692fe69c5
Tolik Zinovyev <tolik@algorand.com>
2021-08-31 16:00:46 -0400

    Move database schema out of postgres.

--- cmd/texttosource/main.go
+++ cmd/texttosource/main.go
@@ -8,34 +8,37 @@ import (
 )
 
 // usage:
-// go run cmd/texttosource/main.go packagename text.file.suffix ...
-//
-// outputs text_file_suffix.go containing constant text_file_suffix
+// go run cmd/texttosource/main.go packagename constantname inputfile outputfile
 func main() {
 	packageName := os.Args[1]
-	for _, fname := range os.Args[2:] {
-		data, err := ioutil.ReadFile(fname)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: %v\n", fname, err)
-			os.Exit(1)
-		}
-		outname := strings.ReplaceAll(fname, ".", "_") + ".go"
-		fout, err := os.Create(outname)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: %v\n", outname, err)
-			os.Exit(1)
-		}
-		varname := strings.ReplaceAll(fname, ".", "_")
-		bodyConstant := "`" + strings.ReplaceAll(string(data), "`", "\\u0060") + "`"
-		_, err = fmt.Fprintf(fout, `// Code generated from source %s via go generate. DO NOT EDIT.
+	constantName := os.Args[2]
+	inputFilepath := os.Args[3]
+	outputFilepath := os.Args[4]
+
+	data, err := ioutil.ReadFile(inputFilepath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot read file %s: %v\n", inputFilepath, err)
+		os.Exit(1)
+	}
+
+	fout, err := os.Create(outputFilepath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot open %s for writing: %v\n", outputFilepath, err)
+		os.Exit(1)
+	}
+
+	body := "`" + strings.ReplaceAll(string(data), "`", "\\u0060") + "`"
+	format := `// Code generated from source %s via go generate. DO NOT EDIT.
 
 package %s
 
 const %s = %s
-`, fname, packageName, varname, bodyConstant)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to write to %s: %v\n", outname, err)
-			os.Exit(1)
-		}
+`
+	_, err = fmt.Fprintf(fout, format, inputFilepath, packageName, constantName, body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to write to %s: %v\n", outputFilepath, err)
+		os.Exit(1)
 	}
+
+	fout.Close()
 }
---
+++ idb/postgres/internal/schema/schema.go
@@ -0,0 +1,3 @@
+package schema
+
+//go:generate go run ../../../../cmd/texttosource/main.go schema SetupPostgresSql setup_postgres.sql setup_postgres_sql.go
