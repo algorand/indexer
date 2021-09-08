5d94cb9e640a7edbd063c65d8b5cfed8ba2ef2be
Tolik Zinovyev <tolik@algorand.com>
2021-09-08 14:37:15 -0400

    Use "pgx" when opening a database (#637)
    * Use "pgx" when opening a database.
    
    * Import pgx.

--- idb/postgres/internal/testing/testing.go
+++ idb/postgres/internal/testing/testing.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"testing"
 
+	// Load the postgres sql.DB implementation.
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/orlangure/gnomock"
 	"github.com/orlangure/gnomock/preset/postgres"
 	"github.com/stretchr/testify/require"
@@ -24,8 +26,8 @@ func SetupPostgres(t *testing.T) (*sql.DB, string, func()) {
 		}
 		connStr := *testpg
 
-		db, err := sql.Open("postgres", connStr)
-		require.NoError(t, err, "Error opening pg connection")
+		db, err := sql.Open("pgx", connStr)
+		require.NoError(t, err, "Error opening postgres connection")
 
 		_, err = db.Exec(`DROP SCHEMA public CASCADE; CREATE SCHEMA public;`)
 		require.NoError(t, err)
@@ -52,8 +54,8 @@ func SetupPostgres(t *testing.T) (*sql.DB, string, func()) {
 		"gnomock", "gnomick", "mydb",
 	)
 
-	db, err := sql.Open("postgres", connStr)
-	require.NoError(t, err, "Error opening pg connection")
+	db, err := sql.Open("pgx", connStr)
+	require.NoError(t, err, "Error opening postgres connection")
 
 	return db, connStr, shutdownFunc
 }
