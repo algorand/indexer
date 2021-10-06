66ff5ffd48c49df2540afd5d1bdde648550d6b03
Tolik Zinovyev <tolik@algorand.com>
2021-10-06 12:05:30 -0400

    Restore MigrationState.

--- idb/postgres/internal/types/types.go
+++ idb/postgres/internal/types/types.go
@@ -8,4 +8,14 @@ type ImportState struct {
 // MigrationState is metadata used by the postgres migrations.
 type MigrationState struct {
 	NextMigration int `json:"next"`
+
+	// The following are deprecated.
+	NextRound    int64  `json:"round,omitempty"`
+	NextAssetID  int64  `json:"assetid,omitempty"`
+	PointerRound *int64 `json:"pointerRound,omitempty"`
+	PointerIntra *int64 `json:"pointerIntra,omitempty"`
+
+	// Note: a generic "data" field here could be a good way to deal with this growing over time.
+	//       It would require a mechanism to clear the data field between migrations to avoid using migration data
+	//       from the previous migration.
 }
