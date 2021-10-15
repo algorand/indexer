6f0ebc225158ef01c0d90f9a8e733f68095ae3b0
algochoi <86622919+algochoi@users.noreply.github.com>
2021-10-14 21:39:24 -0400

    Delete Null characters from Logs (#739)
    This commit strips null characters from logs, which should fix indexers from crashing.
    
    Closes #735

--- idb/postgres/internal/encoding/encode.go
+++ idb/postgres/internal/encoding/encode.go
@@ -2,6 +2,8 @@ package encoding
 
 import (
 	"encoding/base64"
+	"unicode"
+	"unicode/utf8"
 
 	sdk_types "github.com/algorand/go-algorand-sdk/types"
 	"github.com/algorand/go-codec/codec"
@@ -81,7 +83,33 @@ func convertLocalDeltas(deltas map[uint64]types.StateDelta) map[uint64]types.Sta
 	return res
 }
 
+// printableUTF8OrEmpty checks to see if the entire string is a UTF8 printable string.
+// If this is the case, the string is returned as is. Otherwise, the empty string is returned.
+func printableUTF8OrEmpty(in string) string {
+	// iterate throughout all the characters in the string to see if they are all printable.
+	// when range iterating on go strings, go decode each element as a utf8 rune.
+	for _, c := range in {
+		// is this a printable character, or invalid rune ?
+		if c == utf8.RuneError || !unicode.IsPrint(c) {
+			return ""
+		}
+	}
+	return in
+}
+
+func removeNonUTF8Chars(logs []string) []string {
+	if logs == nil {
+		return nil
+	}
+	res := make([]string, len(logs))
+	for i, log := range logs {
+		res[i] = printableUTF8OrEmpty(log)
+	}
+	return res
+}
+
 func convertEvalDelta(evalDelta types.EvalDelta) types.EvalDelta {
+	evalDelta.Logs = removeNonUTF8Chars(evalDelta.Logs)
 	evalDelta.GlobalDelta = convertStateDelta(evalDelta.GlobalDelta)
 	evalDelta.LocalDeltas = convertLocalDeltas(evalDelta.LocalDeltas)
 	return evalDelta
