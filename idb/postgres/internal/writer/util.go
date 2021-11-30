4ea7c883f89c64c8bed8a68060397300ad20724a
Tolik Zinovyev <tolik@algorand.com>
2021-11-30 16:54:56 -0500

    Prepare and write txn rows in parallel.

---
+++ idb/postgres/internal/writer/util.go
@@ -0,0 +1,29 @@
+package writer
+
+import (
+	"github.com/jackc/pgx/v4"
+)
+
+type copyFromChannelStruct struct {
+	ch   chan []interface{}
+	next []interface{}
+	err  error
+}
+
+func (c *copyFromChannelStruct) Next() bool {
+	var ok bool
+	c.next, ok = <-c.ch
+	return ok
+}
+
+func (c *copyFromChannelStruct) Values() ([]interface{}, error) {
+	return c.next, nil
+}
+
+func (c *copyFromChannelStruct) Err() error {
+	return nil
+}
+
+func copyFromChannel(ch chan []interface{}) pgx.CopyFromSource {
+	return &copyFromChannelStruct{ch: ch}
+}
