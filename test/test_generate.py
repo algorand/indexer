faf764fb5d43681a8544dcf2cdbf52a090d7ccc1
Stephen Akiki <stephen.akiki@algorand.com>
2022-01-19 17:30:49 -0500

    Create Python Test for Generated Files
    Resolves #405
    
    Adds a python script that can be used to test that go generate did not
    modify any files

---
+++ test/test_generate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import os
+import sys
+from subprocess import check_output
+
+# Validates that certain files are not modified after running "go generate"
+
+# List of directories that contain generate.go files
+list_of_directories=["../idb/postgres/internal/schema"]
+
+for dir in list_of_directories:
+    os.system("go generate {}".format(dir))
+
+out = check_output(["git status --porcelain | wc -l"], shell=True).strip().decode('UTF-8')
+
+if out != 0:
+    print("Number of files modified was not 0.  It was {}".format(out))
+    out = check_output(["git status --porcelain"], shell=True).strip().decode('UTF-8')
+    print("Output of 'git status --porcelain':\n {}".format(out))
+    sys.exit(1)
