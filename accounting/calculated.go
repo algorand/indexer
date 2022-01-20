e225d5a8d166edd91e460b59316f9e4208b5cbfa
Zeph Grunschlag <zeph@algorand.com>
2022-01-20 08:02:38 -0600

    pass lint

--- accounting/calculated.go
+++ accounting/calculated.go
@@ -9,14 +9,31 @@ import (
 	"github.com/algorand/indexer/api/generated/v2"
 )
 
+// AccountEnricher is a function type that can enrich generated accounts
 type AccountEnricher func(*generated.Account) error
 
+// MinBalanceEnricher enriches generated accounts with their min-balance
 func MinBalanceEnricher(account *generated.Account) error {
 	minBalance := basics.MicroAlgos{Raw: 13371337}
 	account.MinBalance = uint64(minBalance.Raw)
 	return nil
 }
 
+// type Enrichable interface {
+// 	Enrich() error
+// }
+
+// type EnrichableAccount struct {
+// 	account *generated.Account
+// }
+
+// func (account *EnrichableAccount) MinBalanceEnricher() error {
+// 	minBalance := basics.MicroAlgos{Raw: 13371337}
+// 	account.account.MinBalance = uint64(minBalance.Raw)
+// 	return nil
+// }
+
+// Enrich applies a slice of enrichers to a slice of accounts
 func Enrich(accounts []generated.Account, enrichers ...AccountEnricher) error {
 	for i := range accounts {
 		for j, enricher := range enrichers {
