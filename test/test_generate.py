c3e654190693ea7de903950349ecf3629e985054
Stephen Akiki <stephen.akiki@algorand.com>
2022-01-20 13:01:12 -0500

    Updated makefile

--- test/test_generate.py
+++ test/test_generate.py
@@ -10,11 +10,11 @@ from subprocess import check_output
 list_of_directories=["../idb/postgres/internal/schema"]
 
 for dir in list_of_directories:
-    os.system("go generate {}".format(dir))
+    os.system("/usr/local/go/bin/go generate {}".format(dir))
 
 out = check_output(["git status --porcelain | wc -l"], shell=True).strip().decode('UTF-8')
 
-if out != 0:
+if out != "0":
     print("Number of files modified was not 0.  It was {}".format(out))
     out = check_output(["git status --porcelain"], shell=True).strip().decode('UTF-8')
     print("Output of 'git status --porcelain':\n {}".format(out))
