e28e1d0d2df187e58a90a47b2577fba478fc0640
Zeph Grunschlag <zeph@algorand.com>
2022-01-25 14:59:03 -0600

    merge

--- idb/postgres/postgres_integration_test.go
+++ idb/postgres/postgres_integration_test.go
@@ -1876,3 +1876,28 @@ func TestTransactionsTxnAhead(t *testing.T) {
 		require.NoError(t, row.Error)
 	}
 }
+
+// Test that if genesis hash is different from what is in db metastate
+// indexer does not start.
+func TestGenesisHashCheckAtDBStartup(t *testing.T) {
+	_, connStr, shutdownFunc := pgtest.SetupPostgres(t)
+	defer shutdownFunc()
+	genesis := test.MakeGenesis()
+	db := setupIdbWithConnectionString(
+		t, connStr, genesis, test.MakeGenesisBlock())
+	defer db.Close()
+	genesisHash := crypto.HashObj(genesis)
+	network, err := db.getMetastate(context.Background(), nil, schema.NetworkMetaStateKey)
+	assert.NoError(t, err)
+	networkState, err := encoding.DecodeNetworkState([]byte(network))
+	assert.NoError(t, err)
+	assert.Equal(t, genesisHash, networkState.GenesisHash)
+	// connect with different genesis configs
+	genesis.Network = "testnest"
+	// different genesisHash, should fail
+	idb, _, err := OpenPostgres(connStr, idb.IndexerDbOptions{}, nil)
+	assert.NoError(t, err)
+	err = idb.LoadGenesis(genesis)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "genesis hash not matching")
+}
