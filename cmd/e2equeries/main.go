2aad7dbf5a4f368e3804b6995ae8dc41dd939a6a
shiqizng <80276844+shiqizng@users.noreply.github.com>
2022-02-25 17:00:37 -0500

    Fix e2e test to support inner transactions. (#899)

--- cmd/e2equeries/main.go
+++ cmd/e2equeries/main.go
@@ -44,7 +44,12 @@ func main() {
 	rowchan, _ := db.Transactions(context.Background(), rekeyTxnQuery)
 	for txnrow := range rowchan {
 		maybeFail(txnrow.Error, "err rekey txn %v\n", txnrow.Error)
-		rekeyedAuthAddrs = append(rekeyedAuthAddrs, txnrow.Txn.Txn.RekeyTo)
+
+		t := txnrow.Txn
+		if txnrow.RootTxn != nil {
+			t = txnrow.RootTxn
+		}
+		rekeyedAuthAddrs = append(rekeyedAuthAddrs, t.Txn.RekeyTo)
 	}
 
 	// some rekeys get rekeyed back; some rekeyed accounts get deleted, just want to find at least one
@@ -63,7 +68,7 @@ func main() {
 	}
 	if !foundRekey {
 		// this will report the error in a handy way
-		printAccountQuery(db, idb.AccountQueryOptions{EqualToAuthAddr: rekeyedAuthAddrs[0][:], Limit: 1})
+		printAccountQuery(db, idb.AccountQueryOptions{EqualToAuthAddr: rekeyedAuthAddrs[0][:], Limit: 0})
 	}
 
 	// find an asset with > 1 account
