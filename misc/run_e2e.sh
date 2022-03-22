477b78a5b0fde16c71e896a18cf46877eab51d5c
Stephen Akiki <stephen.akiki@algorand.com>
2022-03-22 15:46:15 -0400

    Temp

--- misc/run_e2e.sh
+++ misc/run_e2e.sh
@@ -3,6 +3,13 @@
 set -x
 set -e
 
+# Show me the version
+python3 --version
+
+pushd ../third_party/go-algorand || exit
+make && make install
+popd
+
 CUR_DIR=$(pwd)
 
 # We need to have a local directory as temp because
@@ -32,18 +39,15 @@ mkdir -p "$LOCALBINDIR"
 rm -rf "$LOCALDATADIR"
 mkdir -p "$LOCALDATADIR"
 
-pushd "$LOCALBINDIR" || exit
-curl https://raw.githubusercontent.com/algorand/go-algorand/1e1474216421da27008726c44ebe0a5ba2fb6a08/cmd/updater/update.sh -o update.sh
-chmod +x update.sh
-./update.sh -i -c nightly -p "${LOCALBINDIR}" -d "${LOCALDATADIR}" -n
-popd
-
-export PATH="${LOCALBINDIR}:${PATH}"
-
 rm -rf ve3
 python3 -m venv ve3
-ve3/bin/pip install py-algorand-sdk
+ve3/bin/pip install --upgrade pip
+ve3/bin/pip install --upgrade cryptography
+ve3/bin/pip install py-algorand-sdk==1.10.0
+ve3/bin/pip install --upgrade debugpy
 . ve3/bin/activate
 ./buildtestdata.sh
 
 docker-compose build --build-arg GO_IMAGE=${GO_IMAGE} && docker-compose up --exit-code-from e2e
+# Docker down and remove all images and volumes
+docker-compose down --rmi all -v
