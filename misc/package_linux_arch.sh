5111880401faf45849bcb373a20cad3d2a10a307
Barbara Poon <barbara.poon@algorand.com>
2022-04-29 10:40:53 -0400

    be a little more specific for the safe.directory

--- misc/package_linux_arch.sh
+++ misc/package_linux_arch.sh
@@ -57,4 +57,4 @@ docker run ${DOCKER_RUN_OPTS} \
     -v `pwd`:/go/src/github.com/algorand/indexer \
     --workdir /go/src/github.com/algorand/indexer \
     "${BUILD_IMAGE}" \
-    bash -c "git config --global --add safe.directory '*' && make ${MAKE_TARGET}"
+    bash -c "git config --global --add safe.directory '/go/src/github.com/algorand/indexer/*' && make ${MAKE_TARGET}"
