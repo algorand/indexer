ec40d5ae3d3f9ab8864aa31b286d3bfaff2edc01
shiqi.zheng@algorand.com <shiqi.zheng@algorand.com>
2022-05-03 11:52:28 -0400

    add description

--- processor/processor.go
+++ processor/processor.go
@@ -14,6 +14,7 @@ import (
 	"github.com/algorand/go-algorand/rpcs"
 )
 
+// Processor is the block processor interface
 type Processor interface {
 	Process(cert *rpcs.EncodedBlockCert) error
 	SetHandler(handler func(block *ledgercore.ValidatedBlock) error)
