437e6916b66b2e08a8e3fd6cc3a809b2ec1f5c01
Eric Warehime <eric.warehime@gmail.com>
2022-07-06 11:27:29 -0700

    add noop exporter

---
+++ exporters/noop/noop_exporter_test.go
@@ -0,0 +1,48 @@
+package noop
+
+import (
+	"github.com/algorand/go-algorand/data/bookkeeping"
+	"github.com/algorand/indexer/exporters"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var nc = &NoopConstructor{}
+
+var ne = nc.New()
+
+func TestExporterMetadata(t *testing.T) {
+	meta := ne.Metadata()
+	assert.Equal(t, NoopExporterMetadata.Name, meta.Name)
+	assert.Equal(t, NoopExporterMetadata.Description, meta.Description)
+	assert.Equal(t, NoopExporterMetadata.Deprecated, meta.Deprecated)
+}
+
+func TestExporterConnect(t *testing.T) {
+	assert.NoError(t, ne.Connect(""))
+}
+
+func TestExporterConfig(t *testing.T) {
+	assert.Equal(t, exporters.ExporterConfig(""), ne.Config())
+}
+
+func TestExporterDisconnect(t *testing.T) {
+	assert.NoError(t, ne.Disconnect())
+}
+
+func TestExporterHandleGenesis(t *testing.T) {
+	assert.NoError(t, ne.HandleGenesis(bookkeeping.Genesis{}))
+}
+
+func TestExporterRoundReceive(t *testing.T) {
+	eData := &exporters.BlockExportData{
+		Block: bookkeeping.Block{
+			BlockHeader: bookkeeping.BlockHeader{
+				Round: 5,
+			},
+		},
+	}
+	assert.Equal(t, uint64(0), ne.Round())
+	assert.NoError(t, ne.Receive(eData))
+	assert.Equal(t, uint64(6), ne.Round())
+}
