1760cb62583c45173f33f53a519267cd17efee69
Zeph Grunschlag <zeph@algorand.com>
2022-07-29 11:13:21 -0500

    Format misc/*.py with `black`

--- misc/liveindextest.py
+++ misc/liveindextest.py
@@ -38,45 +38,52 @@ from util import xrun, atexitrun, find_indexer, ensure_test_db
 
 logger = logging.getLogger(__name__)
 
+
 def find_go_algorand():
-    gopath = os.getenv('GOPATH')
-    for path in gopath.split(':'):
-        goa = os.path.join(path, 'src', 'github.com', 'algorand', 'go-algorand')
+    gopath = os.getenv("GOPATH")
+    for path in gopath.split(":"):
+        goa = os.path.join(path, "src", "github.com", "algorand", "go-algorand")
         if os.path.isdir(goa):
             return goa
     return None
 
+
 already_stopped = False
 already_deleted = False
+
+
 def goal_network_stop(netdir, normal_cleanup=False):
     global already_stopped, already_deleted
     if already_stopped or already_deleted:
         return
 
-    logger.info('stop network in %s', netdir)
+    logger.info("stop network in %s", netdir)
     try:
-        xrun(['goal', 'network', 'stop', '-r', netdir], timeout=10)
+        xrun(["goal", "network", "stop", "-r", netdir], timeout=10)
     except Exception as e:
-        logger.error('error stopping network', exc_info=True)
+        logger.error("error stopping network", exc_info=True)
         if normal_cleanup:
             raise e
     already_stopped = True
 
+
 def openkmd(algodata):
-    kmdnetpath = sorted(glob.glob(os.path.join(algodata,'kmd-*','kmd.net')))[-1]
-    kmdnet = open(kmdnetpath, 'rt').read().strip()
-    kmdtokenpath = sorted(glob.glob(os.path.join(algodata,'kmd-*','kmd.token')))[-1]
-    kmdtoken = open(kmdtokenpath, 'rt').read().strip()
-    kmd = algosdk.kmd.KMDClient(kmdtoken, 'http://' + kmdnet)
+    kmdnetpath = sorted(glob.glob(os.path.join(algodata, "kmd-*", "kmd.net")))[-1]
+    kmdnet = open(kmdnetpath, "rt").read().strip()
+    kmdtokenpath = sorted(glob.glob(os.path.join(algodata, "kmd-*", "kmd.token")))[-1]
+    kmdtoken = open(kmdtokenpath, "rt").read().strip()
+    kmd = algosdk.kmd.KMDClient(kmdtoken, "http://" + kmdnet)
     return kmd
 
+
 def openalgod(algodata):
-    algodnetpath = os.path.join(algodata,'algod.net')
-    algodnet = open(algodnetpath, 'rt').read().strip()
-    algodtokenpath = os.path.join(algodata,'algod.token')
-    algodtoken = open(algodtokenpath, 'rt').read().strip()
-    algod = algosdk.algod.AlgodClient(algodtoken, 'http://' + algodnet)
+    algodnetpath = os.path.join(algodata, "algod.net")
+    algodnet = open(algodnetpath, "rt").read().strip()
+    algodtokenpath = os.path.join(algodata, "algod.token")
+    algodtoken = open(algodtokenpath, "rt").read().strip()
+    algod = algosdk.algod.AlgodClient(algodtoken, "http://" + algodnet)
     return algod
+
 
 class RunContext:
     def __init__(self, env):
@@ -96,8 +103,8 @@ class RunContext:
         if self.algod and self.kmd:
             return
         # should run from inside self.lock
-        xrun(['goal', 'kmd', 'start', '-t', '200'], env=self.env, timeout=5)
-        algodata = self.env['ALGORAND_DATA']
+        xrun(["goal", "kmd", "start", "-t", "200"], env=self.env, timeout=5)
+        algodata = self.env["ALGORAND_DATA"]
         self.kmd = openkmd(algodata)
         self.algod = openalgod(algodata)
 
@@ -109,18 +116,18 @@ class RunContext:
                 wallets = self.kmd.list_wallets()
                 pubwid = None
                 for xw in wallets:
-                    if xw['name'] == 'unencrypted-default-wallet':
-                        pubwid = xw['id']
-                pubw = self.kmd.init_wallet_handle(pubwid, '')
+                    if xw["name"] == "unencrypted-default-wallet":
+                        pubwid = xw["id"]
+                pubw = self.kmd.init_wallet_handle(pubwid, "")
                 pubaddrs = self.kmd.list_keys(pubw)
                 pubbalances = []
                 maxamount = 0
                 maxpubaddr = None
                 for pa in pubaddrs:
                     pai = self.algod.account_info(pa)
-                    if pai['amount'] > maxamount:
-                        maxamount = pai['amount']
-                        maxpubaddr = pai['address']
+                    if pai["amount"] > maxamount:
+                        maxamount = pai["amount"]
+                        maxpubaddr = pai["address"]
                 self.pubw = pubw
                 self.maxpubaddr = maxpubaddr
             return self.pubw, self.maxpubaddr
@@ -131,33 +138,61 @@ class RunContext:
 
         # create a wallet with an addr to send to
         walletname = base64.b16encode(os.urandom(16)).decode()
-        winfo = kmd.create_wallet(walletname, '')
-        handle = kmd.init_wallet_handle(winfo['id'], '')
+        winfo = kmd.create_wallet(walletname, "")
+        handle = kmd.init_wallet_handle(winfo["id"], "")
         addr = kmd.generate_key(handle)
 
         # send one million Algos to the test wallet's account
         params = algod.suggested_params()
-        round = params['lastRound']
-        txn = algosdk.transaction.PaymentTxn(sender=maxpubaddr, fee=params['minFee'], first=round, last=round+100, gh=params['genesishashb64'], receiver=addr, amt=1000000000000, flat_fee=True)
-        stxn = kmd.sign_transaction(pubw, '', txn)
+        round = params["lastRound"]
+        txn = algosdk.transaction.PaymentTxn(
+            sender=maxpubaddr,
+            fee=params["minFee"],
+            first=round,
+            last=round + 100,
+            gh=params["genesishashb64"],
+            receiver=addr,
+            amt=1000000000000,
+            flat_fee=True,
+        )
+        stxn = kmd.sign_transaction(pubw, "", txn)
         txid = algod.send_transaction(stxn)
         for i in range(50):
             txinfo = algod.pending_transaction_info(txid)
-            if txinfo.get('round'):
+            if txinfo.get("round"):
                 break
             time.sleep(0.1)
         return txid, txinfo
 
+
 def main():
     start = time.time()
     import argparse
+
     ap = argparse.ArgumentParser()
-    ap.add_argument('--go-algorand', help='path to go-algorand checkout')
-    ap.add_argument('--keep-temps', default=False, action='store_true', help='if set, keep all the test files')
-    ap.add_argument('--indexer-bin', default=None, help='path to algorand-indexer binary, otherwise search PATH')
-    ap.add_argument('--indexer-port', default=None, type=int, help='port to run indexer on. defaults to random in [4000,30000]')
-    ap.add_argument('--connection-string', help='Use this connection string instead of attempting to manage a local database.')
-    ap.add_argument('--verbose', default=False, action='store_true')
+    ap.add_argument("--go-algorand", help="path to go-algorand checkout")
+    ap.add_argument(
+        "--keep-temps",
+        default=False,
+        action="store_true",
+        help="if set, keep all the test files",
+    )
+    ap.add_argument(
+        "--indexer-bin",
+        default=None,
+        help="path to algorand-indexer binary, otherwise search PATH",
+    )
+    ap.add_argument(
+        "--indexer-port",
+        default=None,
+        type=int,
+        help="port to run indexer on. defaults to random in [4000,30000]",
+    )
+    ap.add_argument(
+        "--connection-string",
+        help="Use this connection string instead of attempting to manage a local database.",
+    )
+    ap.add_argument("--verbose", default=False, action="store_true")
     args = ap.parse_args()
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
@@ -170,58 +205,82 @@ def main():
     # env for child processes
     env = dict(os.environ)
 
-    tempdir = os.getenv('TEMPDIR')
+    tempdir = os.getenv("TEMPDIR")
     if not tempdir:
         tempdir = tempfile.mkdtemp()
-        env['TEMPDIR'] = tempdir
-        logger.info('created TEMPDIR %r', tempdir)
+        env["TEMPDIR"] = tempdir
+        logger.info("created TEMPDIR %r", tempdir)
         if not args.keep_temps:
             # If we created a tmpdir and we're not keeping it, clean it up.
             # If an outer process specified $TEMPDIR, let them clean it up.
             atexit.register(shutil.rmtree, tempdir, onerror=logger.error)
         else:
-            atexit.register(print, 'keeping temps. to clean up:\nrm -rf {}'.format(tempdir))
+            atexit.register(
+                print, "keeping temps. to clean up:\nrm -rf {}".format(tempdir)
+            )
 
-    netdir = os.path.join(tempdir, 'net')
-    env['NETDIR'] = netdir
+    netdir = os.path.join(tempdir, "net")
+    env["NETDIR"] = netdir
 
-    template = os.path.join(goalgorand, 'test/testdata/nettemplates/TwoNodes50EachFuture.json')
-    xrun(['goal', 'network', 'create', '-r', netdir, '-n', 'tbd', '-t', template], timeout=30)
-    xrun(['goal', 'network', 'start', '-r', netdir], timeout=30)
+    template = os.path.join(
+        goalgorand, "test/testdata/nettemplates/TwoNodes50EachFuture.json"
+    )
+    xrun(
+        ["goal", "network", "create", "-r", netdir, "-n", "tbd", "-t", template],
+        timeout=30,
+    )
+    xrun(["goal", "network", "start", "-r", netdir], timeout=30)
     atexit.register(goal_network_stop, netdir)
 
-    algodata = os.path.join(netdir, 'Node')
-    env['ALGORAND_DATA'] = algodata
+    algodata = os.path.join(netdir, "Node")
+    env["ALGORAND_DATA"] = algodata
 
     psqlstring = ensure_test_db(args.connection_string, args.keep_temps)
-    primary = os.path.join(netdir, 'Primary')
-    aiport = args.indexer_port or random.randint(4000,30000)
-    indexer_token = 'security-theater'
-    indexerp = subprocess.Popen([indexer_bin, 'daemon', '--algod', primary, '--postgres', psqlstring, '--dev-mode', '--server', ':{}'.format(aiport), '--token', indexer_token])
+    primary = os.path.join(netdir, "Primary")
+    aiport = args.indexer_port or random.randint(4000, 30000)
+    indexer_token = "security-theater"
+    indexerp = subprocess.Popen(
+        [
+            indexer_bin,
+            "daemon",
+            "--algod",
+            primary,
+            "--postgres",
+            psqlstring,
+            "--dev-mode",
+            "--server",
+            ":{}".format(aiport),
+            "--token",
+            indexer_token,
+        ]
+    )
     atexit.register(indexerp.kill)
 
     rc = RunContext(env)
     txid, txinfo = rc.do_txn()
-    logger.debug('submitted txid %s, %r', txid, txinfo)
+    logger.debug("submitted txid %s, %r", txid, txinfo)
 
-    indexer = algosdk.v2client.indexer.IndexerClient(indexer_token, 'http://localhost:{}'.format(aiport))
+    indexer = algosdk.v2client.indexer.IndexerClient(
+        indexer_token, "http://localhost:{}".format(aiport)
+    )
     ok = False
     retcode = 1
     for i in range(30):
         result = indexer.search_transactions(txid=txid)
-        logger.debug('seacrh_transactions: %r', result)
-        they = result.get('transactions')
-        if they and they[0].get('confirmed-round'):
-            logger.info('OK: Got txn')
+        logger.debug("seacrh_transactions: %r", result)
+        they = result.get("transactions")
+        if they and they[0].get("confirmed-round"):
+            logger.info("OK: Got txn")
             ok = True
             retcode = 0
             break
         time.sleep(1.0)
 
     dt = time.time() - start
-    ok = (ok and 'OK') or 'FAIL'
+    ok = (ok and "OK") or "FAIL"
     sys.stdout.write("indexer live test {} ({:.1f}s)\n".format(ok, dt))
     return retcode
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())
--- misc/speedtest.py
+++ misc/speedtest.py
@@ -14,37 +14,40 @@ import urllib.parse
 
 logger = logging.getLogger(__name__)
 
-_localhost_rooturl = 'http://localhost:8980'
+_localhost_rooturl = "http://localhost:8980"
 
 # getAccountsPage(pageurl string, accounts set())
 # return nextToken or None
 def getAccountsPage(pageurl, accounts):
     try:
-        logger.debug('GET %r', pageurl)
+        logger.debug("GET %r", pageurl)
         response = urllib.request.urlopen(pageurl)
     except urllib.error.HTTPError as e:
-        logger.error('failed to fetch %r', pageurl)
-        logger.error('msg: %s', e.file.read())
+        logger.error("failed to fetch %r", pageurl)
+        logger.error("msg: %s", e.file.read())
         raise
     except Exception as e:
-        logger.error('%r', pageurl, exc_info=True)
+        logger.error("%r", pageurl, exc_info=True)
         raise
-    if (response.code != 200) or not response.getheader('Content-Type').startswith('application/json'):
+    if (response.code != 200) or not response.getheader("Content-Type").startswith(
+        "application/json"
+    ):
         raise Exception("bad response to {!r}: {}".format(pageurl, response.reason))
     ob = json.loads(response.read())
-    qa = ob.get('accounts',[])
+    qa = ob.get("accounts", [])
     if not qa:
         return None
     for acct in qa:
-        accounts.add(acct['address'])
-    return ob.get('next-token')
+        accounts.add(acct["address"])
+    return ob.get("next-token")
+
 
 # /v2/acccounts
 def getAccountList(rooturl):
     rootparts = urllib.parse.urlparse(rooturl)
     rawurl = list(rootparts)
     accountsurl = list(rootparts)
-    accountsurl[2] = os.path.join(rawurl[2], 'v2', 'accounts')
+    accountsurl[2] = os.path.join(rawurl[2], "v2", "accounts")
     query = {}
     accounts = set()
     start = time.time()
@@ -52,7 +55,7 @@ def getAccountList(rooturl):
     reporturl = None
     while True:
         if nextToken is not None:
-            query['next'] = nextToken
+            query["next"] = nextToken
         if query:
             accountsurl[4] = urllib.parse.urlencode(query)
         pageurl = urllib.parse.urlunparse(accountsurl)
@@ -62,8 +65,14 @@ def getAccountList(rooturl):
         if nextToken is None:
             break
     dt = time.time() - start
-    logger.info('account list: %d accounts in %0.2f seconds, %.1f /s', len(accounts), dt, len(accounts)/dt)
-    return accounts, {'url':reporturl, 'accounts': len(accounts), 'seconds': dt}
+    logger.info(
+        "account list: %d accounts in %0.2f seconds, %.1f /s",
+        len(accounts),
+        dt,
+        len(accounts) / dt,
+    )
+    return accounts, {"url": reporturl, "accounts": len(accounts), "seconds": dt}
+
 
 # /v2/accounts/AAAAA
 # TODO: multithreading, because a single client won't find the limit of the server
@@ -77,13 +86,15 @@ def accountRandom(rooturl, accounts, n=1000, minTime=None, maxTime=30):
     start = time.time()
     reporturl = None
     for addr in accounts:
-        accountsurl[2] = os.path.join(rawurl[2], 'v2', 'accounts', addr)
+        accountsurl[2] = os.path.join(rawurl[2], "v2", "accounts", addr)
         pageurl = urllib.parse.urlunparse(accountsurl)
         if reporturl is None:
             reporturl = pageurl
-        logger.debug('GET %s', pageurl)
+        logger.debug("GET %s", pageurl)
         response = urllib.request.urlopen(pageurl)
-        if (response.code != 200) or not response.getheader('Content-Type').startswith('application/json'):
+        if (response.code != 200) or not response.getheader("Content-Type").startswith(
+            "application/json"
+        ):
             raise Exception("bad response to {!r}: {}".format(pageurl, response.reason))
         # don't actually care about content, just check that it parses
         json.loads(response.read())
@@ -95,8 +106,11 @@ def accountRandom(rooturl, accounts, n=1000, minTime=None, maxTime=30):
             break
         if maxTime and (dt > maxTime):
             break
-    logger.info('account random: %d accounts in %0.2f seconds, %.1f /s', count, dt, count/dt)
-    return {'url':reporturl, 'accounts': count, 'seconds': dt}
+    logger.info(
+        "account random: %d accounts in %0.2f seconds, %.1f /s", count, dt, count / dt
+    )
+    return {"url": reporturl, "accounts": count, "seconds": dt}
+
 
 # /v2/transactions?addr=
 # equivalent to
@@ -112,18 +126,20 @@ def accountRecents(rooturl, accounts, n=1000, minTime=None, maxTime=10, ntxns=10
     start = time.time()
     reporturl = None
     for addr in accounts:
-        atxnsurl[2] = os.path.join(rawurl[2], 'v2', 'transactions')
-        query = {'limit':ntxns, 'address':addr}
+        atxnsurl[2] = os.path.join(rawurl[2], "v2", "transactions")
+        query = {"limit": ntxns, "address": addr}
         atxnsurl[4] = urllib.parse.urlencode(query)
         pageurl = urllib.parse.urlunparse(atxnsurl)
         if reporturl is None:
             reporturl = pageurl
-        logger.debug('GET %s', pageurl)
+        logger.debug("GET %s", pageurl)
         response = urllib.request.urlopen(pageurl)
-        if (response.code != 200) or not response.getheader('Content-Type').startswith('application/json'):
+        if (response.code != 200) or not response.getheader("Content-Type").startswith(
+            "application/json"
+        ):
             raise Exception("bad response to {!r}: {}".format(pageurl, response.reason))
         ob = json.loads(response.read())
-        txns = ob.get('transactions', [])
+        txns = ob.get("transactions", [])
         txcount += len(txns)
         count += 1
         dt = time.time() - start
@@ -133,15 +149,23 @@ def accountRecents(rooturl, accounts, n=1000, minTime=None, maxTime=10, ntxns=10
             break
         if maxTime and (dt > maxTime):
             break
-    logger.info('account recent txns: %d accounts in %0.2f seconds, %.1f /s; %d txns, %.1f txn/s', count, dt, count/dt, txcount, txcount/dt)
-    return {'url':reporturl, 'accounts': count, 'seconds': dt, 'txns': txcount}
+    logger.info(
+        "account recent txns: %d accounts in %0.2f seconds, %.1f /s; %d txns, %.1f txn/s",
+        count,
+        dt,
+        count / dt,
+        txcount,
+        txcount / dt,
+    )
+    return {"url": reporturl, "accounts": count, "seconds": dt, "txns": txcount}
+
 
 # /v2/assets
 def getAssets(rooturl):
     rootparts = urllib.parse.urlparse(rooturl)
     rawurl = list(rootparts)
     accountsurl = list(rootparts)
-    accountsurl[2] = os.path.join(rawurl[2], 'v2', 'assets')
+    accountsurl[2] = os.path.join(rawurl[2], "v2", "assets")
     query = {}
     assets = {}
     start = time.time()
@@ -149,25 +173,33 @@ def getAssets(rooturl):
     reporturl = None
     while True:
         if nextToken is not None:
-            query['next'] = nextToken
+            query["next"] = nextToken
         if query:
             accountsurl[4] = urllib.parse.urlencode(query)
         pageurl = urllib.parse.urlunparse(accountsurl)
         if reporturl is None:
             reporturl = pageurl
-        logger.debug('GET %s', pageurl)
+        logger.debug("GET %s", pageurl)
         response = urllib.request.urlopen(pageurl)
-        if (response.code != 200) or not response.getheader('Content-Type').startswith('application/json'):
+        if (response.code != 200) or not response.getheader("Content-Type").startswith(
+            "application/json"
+        ):
             raise Exception("bad response to {!r}: {}".format(pageurl, response.reason))
         page = json.loads(response.read())
-        for arec in page.get('assets', []):
-            assets[arec['index']] = arec['params']
-        nextToken = page.get('next-token')
+        for arec in page.get("assets", []):
+            assets[arec["index"]] = arec["params"]
+        nextToken = page.get("next-token")
         if nextToken is None:
             break
     dt = time.time() - start
-    logger.info('asset list: %d assets in %0.2f seconds, %.1f /s', len(assets), dt, len(assets)/dt)
-    return assets, {'url':reporturl, 'assets': len(assets), 'seconds': dt}
+    logger.info(
+        "asset list: %d assets in %0.2f seconds, %.1f /s",
+        len(assets),
+        dt,
+        len(assets) / dt,
+    )
+    return assets, {"url": reporturl, "assets": len(assets), "seconds": dt}
+
 
 # /v2/assets/{asset-id}/transactions
 # equivalent to
@@ -186,18 +218,22 @@ def assetTxns(rooturl, assets, n=1000, minTime=None, maxTime=10, ntxns=1000):
     start = time.time()
     reporturl = None
     for assetid in assets:
-        atxnsurl[2] = os.path.join(rawurl[2], 'v2', 'assets', str(assetid), 'transactions')
-        query = {'limit':ntxns}
+        atxnsurl[2] = os.path.join(
+            rawurl[2], "v2", "assets", str(assetid), "transactions"
+        )
+        query = {"limit": ntxns}
         atxnsurl[4] = urllib.parse.urlencode(query)
         pageurl = urllib.parse.urlunparse(atxnsurl)
         if reporturl is None:
             reporturl = pageurl
-        logger.debug('GET %s', pageurl)
+        logger.debug("GET %s", pageurl)
         response = urllib.request.urlopen(pageurl)
-        if (response.code != 200) or not response.getheader('Content-Type').startswith('application/json'):
+        if (response.code != 200) or not response.getheader("Content-Type").startswith(
+            "application/json"
+        ):
             raise Exception("bad response to {!r}: {}".format(pageurl, response.reason))
         ob = json.loads(response.read())
-        txns = ob.get('transactions', [])
+        txns = ob.get("transactions", [])
         txcount += len(txns)
         count += 1
         dt = time.time() - start
@@ -207,8 +243,16 @@ def assetTxns(rooturl, assets, n=1000, minTime=None, maxTime=10, ntxns=1000):
             break
         if maxTime and (dt > maxTime):
             break
-    logger.info('asset txns: %d assets in %0.2f seconds, %.1f /s; %d txns, %.1f txn/s', count, dt, count/dt, txcount, txcount/dt)
-    return {'url':reporturl, 'assets': count, 'seconds': dt, 'txns': txcount}
+    logger.info(
+        "asset txns: %d assets in %0.2f seconds, %.1f /s; %d txns, %.1f txn/s",
+        count,
+        dt,
+        count / dt,
+        txcount,
+        txcount / dt,
+    )
+    return {"url": reporturl, "assets": count, "seconds": dt, "txns": txcount}
+
 
 # /v2/assets/{asset-id}/balances -- maybe add index to account_asset table?
 #
@@ -225,18 +269,20 @@ def assetBalances(rooturl, assets, n=1000, minTime=None, maxTime=10, ntxns=1000)
     start = time.time()
     reporturl = None
     for assetid in assets:
-        atxnsurl[2] = os.path.join(rawurl[2], 'v2', 'assets', str(assetid), 'balances')
-        query = {'limit':ntxns}
+        atxnsurl[2] = os.path.join(rawurl[2], "v2", "assets", str(assetid), "balances")
+        query = {"limit": ntxns}
         atxnsurl[4] = urllib.parse.urlencode(query)
         pageurl = urllib.parse.urlunparse(atxnsurl)
         if reporturl is None:
             reporturl = pageurl
-        logger.debug('GET %s', pageurl)
+        logger.debug("GET %s", pageurl)
         response = urllib.request.urlopen(pageurl)
-        if (response.code != 200) or not response.getheader('Content-Type').startswith('application/json'):
+        if (response.code != 200) or not response.getheader("Content-Type").startswith(
+            "application/json"
+        ):
             raise Exception("bad response to {!r}: {}".format(pageurl, response.reason))
         ob = json.loads(response.read())
-        bals = ob.get('balances', [])
+        bals = ob.get("balances", [])
         balcount += len(bals)
         count += 1
         dt = time.time() - start
@@ -246,8 +292,16 @@ def assetBalances(rooturl, assets, n=1000, minTime=None, maxTime=10, ntxns=1000)
             break
         if maxTime and (dt > maxTime):
             break
-    logger.info('asset balances: %d assets in %0.2f seconds, %.1f /s; %d bals, %.1f bal/s', count, dt, count/dt, balcount, balcount/dt)
-    return {'url':reporturl, 'assets': count, 'seconds': dt, 'balances': balcount}
+    logger.info(
+        "asset balances: %d assets in %0.2f seconds, %.1f /s; %d bals, %.1f bal/s",
+        count,
+        dt,
+        count / dt,
+        balcount,
+        balcount / dt,
+    )
+    return {"url": reporturl, "assets": count, "seconds": dt, "balances": balcount}
+
 
 # TODO:
 # /v2/applications -- this should be fast because it's a single table scan
@@ -259,13 +313,18 @@ def assetBalances(rooturl, assets, n=1000, minTime=None, maxTime=10, ntxns=1000)
 
 def main():
     import argparse
+
     ap = argparse.ArgumentParser()
-    ap.add_argument('--indexer', default='http://localhost:8980', help='URL to indexer to fetch from, default http://localhost:8980')
-    ap.add_argument('--accounts', type=bool, default=True)
-    ap.add_argument('--no-accounts', dest='accounts', action='store_false')
-    ap.add_argument('--assets', type=bool, default=True)
-    ap.add_argument('--no-assets', dest='assets', action='store_false')
-    ap.add_argument('--verbose', default=False, action='store_true')
+    ap.add_argument(
+        "--indexer",
+        default="http://localhost:8980",
+        help="URL to indexer to fetch from, default http://localhost:8980",
+    )
+    ap.add_argument("--accounts", type=bool, default=True)
+    ap.add_argument("--no-accounts", dest="accounts", action="store_false")
+    ap.add_argument("--assets", type=bool, default=True)
+    ap.add_argument("--no-assets", dest="assets", action="store_false")
+    ap.add_argument("--verbose", default=False, action="store_true")
     args = ap.parse_args()
 
     if args.verbose:
@@ -274,8 +333,8 @@ def main():
         logging.basicConfig(level=logging.INFO)
 
     indexerurl = args.indexer
-    if not indexerurl.startswith('http://'):
-        indexerurl = 'http://' + indexerurl
+    if not indexerurl.startswith("http://"):
+        indexerurl = "http://" + indexerurl
     report = []
 
     if args.accounts:
@@ -293,8 +352,9 @@ def main():
         rept = assetBalances(indexerurl, assets)
         report.append(rept)
     json.dump(report, sys.stdout)
-    sys.stdout.write('\n')
+    sys.stdout.write("\n")
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())
