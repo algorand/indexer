f2b422fd4f736247d138033c00d46e2b94aed484
Eric Warehime <eric.warehime@gmail.com>
2022-08-01 10:09:02 -0700

    docs: Plugin docs (#1150)
    * Update READEME
    
    * Add initial Conduit.md
    
    * Add example exporter, exporter README.md

---
+++ exporters/example/example_exporter_test.go
@@ -0,0 +1,45 @@
+package example
+
+import (
+	"github.com/algorand/go-algorand/data/bookkeeping"
+	"github.com/algorand/indexer/data"
+	"github.com/algorand/indexer/plugins"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var exCons = &Constructor{}
+
+var exExp = exCons.New()
+
+func TestExporterMetadata(t *testing.T) {
+	meta := exExp.Metadata()
+	assert.Equal(t, plugins.PluginType(plugins.Exporter), meta.Type())
+	assert.Equal(t, exampleExporterMetadata.ExpName, meta.Name())
+	assert.Equal(t, exampleExporterMetadata.ExpDescription, meta.Description())
+	assert.Equal(t, exampleExporterMetadata.ExpDeprecated, meta.Deprecated())
+}
+
+func TestExporterInit(t *testing.T) {
+	assert.Panics(t, func() { exExp.Init("", nil) })
+}
+
+func TestExporterConfig(t *testing.T) {
+	assert.Panics(t, func() { exExp.Config() })
+}
+
+func TestExporterClose(t *testing.T) {
+	assert.Panics(t, func() { exExp.Close() })
+}
+
+func TestExporterReceive(t *testing.T) {
+	assert.Panics(t, func() { exExp.Receive(data.BlockData{}) })
+}
+
+func TestExporterHandleGenesis(t *testing.T) {
+	assert.Panics(t, func() { exExp.HandleGenesis(bookkeeping.Genesis{}) })
+}
+
+func TestExporterRound(t *testing.T) {
+	assert.Panics(t, func() { exExp.Round() })
+}
