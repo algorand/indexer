e6724afb12870cfbdbc0fe23a3af8f082bb19ae6
Stephen Akiki <stephen.akiki@algorand.com>
2022-08-10 09:18:26 -0400

    Modify bindflagset for conduit

--- cmd/conduit/main.go
+++ cmd/conduit/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"os"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/algorand/indexer/conduit"
-	"github.com/algorand/indexer/config"
 )
 
 import (
@@ -44,7 +46,21 @@ func init() {
 func runConduitCmdWithConfig(cfg *conduit.Config) error {
 
 	// Tie cobra variables into local go-lang variables
-	config.BindFlagSet(cfg.Flags)
+	cfg.Flags.VisitAll(func(f *pflag.Flag) {
+		// Environment variables can't have dashes in them, so bind them to their equivalent
+		// keys with underscores
+		// e.g. prefix=STING and --favorite-color is set to STING_FAVORITE_COLOR
+		if strings.Contains(f.Name, "-") {
+			envVarSuffix := strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_"))
+			viper.BindEnv(f.Name, fmt.Sprintf("%s_%s", "CONDUIT", envVarSuffix))
+		}
+
+		// Apply the viper config value to the flag when the flag is not set and viper has a value
+		if !f.Changed && viper.IsSet(f.Name) {
+			val := viper.Get(f.Name)
+			cfg.Flags.Set(f.Name, fmt.Sprintf("%v", val))
+		}
+	})
 	logger.Info(cfg)
 
 	if err := cfg.Valid(); err != nil {
