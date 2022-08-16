b129605cb39b55e03a42d0959a7eb4f450909a24
Will Winder <wwinder.unh@gmail.com>
2022-08-16 17:05:37 -0400

    Disable deadlock detection (#1186)

--- cmd/algorand-indexer/main.go
+++ cmd/algorand-indexer/main.go
@@ -17,6 +17,7 @@ import (
 	"github.com/algorand/indexer/idb"
 	"github.com/algorand/indexer/idb/dummy"
 	_ "github.com/algorand/indexer/idb/postgres"
+	_ "github.com/algorand/indexer/util/disabledeadlock"
 	"github.com/algorand/indexer/util/metrics"
 	"github.com/algorand/indexer/version"
 )
