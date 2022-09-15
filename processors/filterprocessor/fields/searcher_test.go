0fdcf5035e651fd933fe4321914fea7815afb204
Stephen Akiki <stephen.akiki@algorand.com>
2022-09-15 12:14:22 -0400

    Test failure fix

--- processors/filterprocessor/fields/searcher_test.go
+++ processors/filterprocessor/fields/searcher_test.go
@@ -9,13 +9,13 @@ import (
 // TestCheckTagExistsAndHasCorrectFunction tests that the check tag exists and function relation works
 func TestCheckTagExistsAndHasCorrectFunction(t *testing.T) {
 	// check that something that doesn't exist throws an error
-	err := checkTagExistsAndHasCorrectFunction("const", "SignedTxnWithAD.SignedTxn.Txn.PaymentTxnFields.LoreumIpsum.SDF")
+	err := checkTagExistsAndHasCorrectFunction("exact", "SignedTxnWithAD.SignedTxn.Txn.PaymentTxnFields.LoreumIpsum.SDF")
 	assert.ErrorContains(t, err, "does not exist in transactions")
 
-	err = checkTagExistsAndHasCorrectFunction("const", "LoreumIpsum")
+	err = checkTagExistsAndHasCorrectFunction("exact", "LoreumIpsum")
 	assert.ErrorContains(t, err, "does not exist in transactions")
 
 	// Fee does not have a "String" Function so we cant use const with it.
-	err = checkTagExistsAndHasCorrectFunction("const", "SignedTxnWithAD.SignedTxn.Txn.Header.Fee")
+	err = checkTagExistsAndHasCorrectFunction("exact", "SignedTxnWithAD.SignedTxn.Txn.Header.Fee")
 	assert.ErrorContains(t, err, "does not contain the needed method")
 }
