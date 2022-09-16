6c18d483ffb1611f4756ba5e23fe9a446f69c55d
Stephen Akiki <stephen.akiki@algorand.com>
2022-09-16 11:12:50 -0400

    Config comments

--- processors/filterprocessor/config.go
+++ processors/filterprocessor/config.go
@@ -4,15 +4,16 @@ import "github.com/algorand/indexer/processors/filterprocessor/expression"
 
 // SubConfig is the configuration needed for each additional filter
 type SubConfig struct {
-	// The tag of the struct to analyze
+	// FilterTag the tag of the struct to analyze
 	FilterTag string `yaml:"tag"`
-	// The type of expression to search for "const" or "regex"
+	// ExpressionType the type of expression to search for (i.e. "exact" or "regex")
 	ExpressionType expression.FilterType `yaml:"expression-type"`
-	// The expression to search
+	// Expression the expression to search
 	Expression string `yaml:"expression"`
 }
 
 // Config configuration for the filter processor
 type Config struct {
+	// Filters are a list of SubConfig objects with an operation acting as the string key in the map
 	Filters []map[string][]SubConfig `yaml:"filters"`
 }
