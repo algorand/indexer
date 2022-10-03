0adb684eeda2d54094c2fad8bf7761e7379151f0
Zeph Grunschlag <zeph@algorand.com>
2022-10-03 17:48:32 -0500

    MakeCatchpointCatchupAccessor

--- processor/blockprocessor/internal/catchupservice.go
+++ processor/blockprocessor/internal/catchupservice.go
@@ -10,6 +10,7 @@ import (
 	"github.com/algorand/go-algorand/catchup"
 	"github.com/algorand/go-algorand/config"
 	"github.com/algorand/go-algorand/data/bookkeeping"
+	"github.com/algorand/go-algorand/ledger"
 	"github.com/algorand/go-algorand/ledger/ledgercore"
 	"github.com/algorand/go-algorand/logging"
 	"github.com/algorand/go-algorand/network"
@@ -97,7 +98,7 @@ func CatchupServiceCatchup(ctx context.Context, logger *log.Logger, catchpoint, 
 		node,
 		wrappedLogger,
 		net,
-		l,
+		ledger.MakeCatchpointCatchupAccessor(l, wrappedLogger),
 		cfg,
 	)
 	if err != nil {
