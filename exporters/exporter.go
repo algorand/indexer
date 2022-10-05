a465340af2fc949a52b5105ac8fe597ca6a010fd
shiqi.zheng@algorand.com <shiqi.zheng@algorand.com>
2022-10-05 13:25:00 -0400

    update exporter interface

--- exporters/exporter.go
+++ exporters/exporter.go
@@ -1,7 +1,6 @@
 package exporters
 
 import (
-	"github.com/algorand/go-algorand/data/bookkeeping"
 	"github.com/algorand/indexer/data"
 	"github.com/algorand/indexer/plugins"
 	"github.com/sirupsen/logrus"
@@ -16,7 +15,7 @@ type Exporter interface {
 	// Typically used for things like initializing network connections.
 	// The ExporterConfig passed to Connect will contain the Unmarhsalled config file specific to this plugin.
 	// Should return an error if it fails--this will result in the Indexer process terminating.
-	Init(cfg plugins.PluginConfig, logger *logrus.Logger) error
+	Init(initProvider data.InitProvider, cfg plugins.PluginConfig, logger *logrus.Logger) error
 
 	// Config returns the configuration options used to create an Exporter.
 	// Initialized during Connect, it should return nil until the Exporter has been Connected.
@@ -30,12 +29,4 @@ type Exporter interface {
 	// Receive is called for each block to be processed by the exporter.
 	// Should return an error on failure--retries are configurable.
 	Receive(exportData data.BlockData) error
-
-	// HandleGenesis is an Exporter's opportunity to do initial validation and handling of the Genesis block.
-	// If validation (such as a check to ensure `genesis` matches a previously stored genesis block) or handling fails,
-	// it returns an error.
-	HandleGenesis(genesis bookkeeping.Genesis) error
-
-	// Round returns the next round not yet processed by the Exporter. Atomically updated when Receive successfully completes.
-	Round() uint64
 }
