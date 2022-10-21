d03e0f842491ef4afe7ffd91af78301add3c8e35
Will Winder <wwinder.unh@gmail.com>
2022-10-21 09:00:20 -0400

    Fix lint problems.

--- conduit/hooks.go
+++ conduit/hooks.go
@@ -2,11 +2,13 @@ package conduit
 
 import "github.com/algorand/indexer/data"
 
+// OnCompleteFunc is the signature for the Completed functional interface.
 type OnCompleteFunc func(input data.BlockData) error
 
+// Completed is called by the conduit pipeline after every exporter has
+// finished. It can be used for things like finalizing state.
 type Completed interface {
 	// OnComplete will be called by the Conduit framework when the pipeline
-	// finishes processing a round. It can be used for things like finalizing
-	// state.
+	// finishes processing a round.
 	OnComplete(input data.BlockData) error
 }
