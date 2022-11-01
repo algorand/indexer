1d23e2d34e88da73a859fa83aaf6e7b84393a876
Stephen Akiki <stephen.akiki@algorand.com>
2022-11-01 13:18:29 -0400

    Better unit tests

--- processors/filterprocessor/sample_test.go
+++ processors/filterprocessor/sample_test.go
@@ -2,7 +2,9 @@ package filterprocessor
 
 import (
 	"context"
+	"gopkg.in/yaml.v3"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/algorand/indexer/conduit"
@@ -12,8 +14,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestSample validates that all fields are set and that the config is valid for the filter processor
-func TestFilterProcessorSampleConfig(t *testing.T) {
+// TestFilterProcessorSampleConfigInit validates that all fields in the sample config are valid for a filter processor
+func TestFilterProcessorSampleConfigInit(t *testing.T) {
 
 	fpBuilder, err := processors.ProcessorBuilderByName("filter_processor")
 	assert.NoError(t, err)
@@ -24,4 +26,22 @@ func TestFilterProcessorSampleConfig(t *testing.T) {
 	fp := fpBuilder.New()
 	err = fp.Init(context.Background(), &conduit.PipelineInitProvider{}, plugins.PluginConfig(sampleConfigStr), logrus.New())
 	assert.NoError(t, err)
+}
+
+// TestFilterProcessorSampleConfigNotEmpty tests that all fields in the sample config are filled in
+func TestFilterProcessorSampleConfigNotEmpty(t *testing.T) {
+
+	sampleConfigStr, err := os.ReadFile("sample.yaml")
+	assert.NoError(t, err)
+	pCfg := Config{}
+
+	err = yaml.Unmarshal(sampleConfigStr, &pCfg)
+	assert.NoError(t, err)
+
+	v := reflect.ValueOf(pCfg)
+
+	for i := 0; i < v.NumField(); i++ {
+		assert.True(t, v.Field(i).Interface() != nil)
+	}
+
 }
