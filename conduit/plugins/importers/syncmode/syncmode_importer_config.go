6dd942c3674ccc4eafa53aa3eedaa323129e0fa4
AlgoStephenAkiki <85183435+AlgoStephenAkiki@users.noreply.github.com>
2023-01-18 19:55:07 -0500

    Update conduit/plugins/importers/syncmode/syncmode_importer_config.go
    Co-authored-by: Eric Warehime <eric.warehime@gmail.com>

--- conduit/plugins/importers/syncmode/syncmode_importer_config.go
+++ conduit/plugins/importers/syncmode/syncmode_importer_config.go
@@ -1,9 +1,13 @@
 package syncmode
 
+//go:generate conduit-docs ../../../../conduit-docs/
+
+//Name: conduit_importers_syncmode
+
 // Config specific to the sync mode importer
 type Config struct {
-	// Algod netaddr string
+	// <code>netaddr</code> is the Algod network address. It must be either an <code>http</code> or <code>https</code> URL.
 	NetAddr string `yaml:"netaddr"`
-	// Algod rest endpoint token
+	// <code>token</code> is the Algod API endpoint token.
 	Token string `yaml:"token"`
 }
