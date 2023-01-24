e732a72282ab20d554b18d7a8f41bb7b75fba07d
Stephen Akiki <stephen.akiki@algorand.com>
2023-01-24 10:44:16 -0500

    Added to all

--- conduit/plugins/importers/all/all.go
+++ conduit/plugins/importers/all/all.go
@@ -4,4 +4,5 @@ import (
 	// Call package wide init function
 	_ "github.com/algorand/indexer/conduit/plugins/importers/algod"
 	_ "github.com/algorand/indexer/conduit/plugins/importers/filereader"
+	_ "github.com/algorand/indexer/conduit/plugins/importers/syncalgod"
 )
