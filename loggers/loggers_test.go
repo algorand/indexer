280558873ff1e80d3708d74214072673879dab56
Will Winder <wwinder.unh@gmail.com>
2023-02-08 14:12:57 -0500

    logging: Fix log-file configuration. (#1453)

--- loggers/loggers_test.go
+++ loggers/loggers_test.go
@@ -3,7 +3,9 @@ package loggers
 import (
 	"encoding/json"
 	"math/rand"
+	"os"
 	"path"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -24,19 +26,19 @@ func (f *FakeIoWriter) Write(p []byte) (n int, err error) {
 }
 
 func TestLogToFile(t *testing.T) {
-	fakeWriter := FakeIoWriter{}
-	lMgr := MakeLoggerManager(&fakeWriter)
-
 	logfile := path.Join(t.TempDir(), "mylogfile.txt")
 	require.NoFileExists(t, logfile)
-	logger, err := lMgr.MakeRootLogger(log.InfoLevel, logfile)
+	logger, err := MakeThreadSafeLogger(log.InfoLevel, logfile)
 	require.NoError(t, err)
 
 	testString := "1234abcd"
 	logger.Infof(testString)
 	assert.FileExists(t, logfile)
-	assert.Len(t, fakeWriter.Entries, 1)
-	assert.Contains(t, fakeWriter.Entries[0], testString)
+	data, err := os.ReadFile(logfile)
+	assert.Contains(t, string(data), testString)
+	lines := strings.Split(string(data), "\n")
+	require.Len(t, lines, 2)
+	assert.Equal(t, "", lines[1])
 }
 
 // TestThreadSafetyOfLogger ensures that multiple threads writing to a single source
@@ -45,13 +47,16 @@ func TestThreadSafetyOfLogger(t *testing.T) {
 	var atomicInt int64 = 0
 
 	fakeWriter := FakeIoWriter{}
-	lMgr := MakeLoggerManager(&fakeWriter)
 
 	const numberOfWritesPerLogger = 20
 	const numberOfLoggers = 15
 
 	var wg sync.WaitGroup
 	wg.Add(numberOfLoggers)
+	writer := ThreadSafeWriter{
+		Writer: &fakeWriter,
+		Mutex:  &sync.Mutex{},
+	}
 
 	// Launch go routines
 	for i := 0; i < numberOfLoggers; i++ {
@@ -59,8 +64,8 @@ func TestThreadSafetyOfLogger(t *testing.T) {
 			// Sleep a random number of milliseconds before and after to test
 			// that creating a logger doesn't affect thread-safety
 			time.Sleep(time.Duration(rand.Intn(50)) * time.Millisecond)
-			l, err := lMgr.MakeRootLogger(log.InfoLevel, "")
-			require.NoError(t, err)
+			l := log.New()
+			l.SetOutput(writer)
 			l.SetFormatter(&log.JSONFormatter{
 				// We want to disable timestamps to stop logrus from sorting our output
 				DisableTimestamp: true,
