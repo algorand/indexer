4617b29e99234c5fa634472233eed01c431fac30
Will Winder <wwinder.unh@gmail.com>
2023-02-10 20:40:17 -0500

    First pass casting to simple types.

--- conduit/plugins/processors/filterprocessor/gen/generate.go
+++ conduit/plugins/processors/filterprocessor/gen/generate.go
@@ -8,23 +8,84 @@ import (
 	"strings"
 	"text/template"
 
+	"github.com/algorand/go-algorand-sdk/v2/types"
+	"github.com/algorand/go-algorand/data/basics"
 	"github.com/algorand/go-algorand/data/transactions"
 )
 
-const InputName = "input"
-
 type StructField struct {
-	Tag      string
-	Path     string
-	CastPre  string
-	CastPost string
+	TagPath    string
+	FieldPath  string
+	CastPrefix string
+	CastPost   string
 }
 
-func (sf StructField) toReturnValue(varName string) string {
-	return fmt.Sprintf("%s(%s.%s)%s", sf.CastPre, varName, sf.Path, sf.CastPost)
+func ReturnValue(sf StructField, varName string) string {
+	return fmt.Sprintf("%s%s.%s%s", sf.CastPrefix, varName, sf.FieldPath, sf.CastPost)
 }
 
-func Cast(t reflect.StructField) (pre string, post string, err error) {
+func noCast(t reflect.StructField) bool {
+	switch reflect.New(t.Type).Elem().Interface().(type) {
+	case uint64:
+		return true
+	case int64:
+		return true
+	case string:
+		return true
+	}
+	return false
+}
+
+func simpleCast(t reflect.StructField) string {
+	switch reflect.New(t.Type).Elem().Interface().(type) {
+	// unsigned
+	case uint:
+		return "uint64"
+	case uint8:
+		return "uint64"
+	case uint16:
+		return "uint64"
+	case uint32:
+		return "uint64"
+	// signed
+	case int:
+		return "int64"
+	case int8:
+		return "int64"
+	case int16:
+		return "int64"
+	case int32:
+		return "int64"
+	// alias
+	case types.MicroAlgos:
+		// SDK microalgo does not need ".Raw"
+		return "uint64"
+
+	}
+	return ""
+
+}
+
+func CastParts(t reflect.StructField) (prefix, postfix string, err error) {
+	if noCast(t) {
+		return
+	}
+
+	if simple := simpleCast(t); simple != "" {
+		prefix = fmt.Sprintf("%s(", simple)
+		postfix = ")"
+		return
+	}
+
+	// all the rest... custom things
+	switch reflect.New(t.Type).Elem().Interface().(type) {
+	case basics.MicroAlgos:
+		prefix = "uint64("
+		postfix = ".Raw)"
+	default:
+		prefix = "NOT "
+		postfix = " HANDLED"
+	}
 	return
 }
 
@@ -58,11 +119,11 @@ func recursiveTagFields(theStruct interface{}, output map[string]StructField, ta
 
 			fullTag := strings.Join(append(tagLevel, tagValue), ".")
 			sf := StructField{
-				Tag:  fullTag,
-				Path: strings.Join(append(fieldLevel, name), "."),
+				TagPath:   fullTag,
+				FieldPath: strings.Join(append(fieldLevel, name), "."),
 			}
 			var err error
-			sf.CastPre, sf.CastPost, err = Cast(field)
+			sf.CastPrefix, sf.CastPost, err = CastParts(field)
 			if err != nil {
 				return fmt.Errorf("problem casting %s: %s", fullTag, err)
 			}
@@ -94,12 +155,11 @@ import (
 
 // LookupFieldByTag takes a tag and associated SignedTxnInBlock and returns the value 
 // referenced by the tag.  An error is returned if the tag does not exist
-func LookupFieldByTag(tag string, {{ .InputName }} *transactions.SignedTxnInBlock) (interface{}, error) {
+func LookupFieldByTag(tag string, input *transactions.SignedTxnInBlock) (interface{}, error) {
 	switch tag {
-{{ range .StructFields }}	case "{{ .Tag }}":
-		return &{{ .Path }}
-{{ end }}
-	default:
+{{ range .StructFields }}	case "{{ .TagPath }}":
+		return {{ ReturnValue . "input" }}
+{{ end }}default:
 		return nil, fmt.Errorf(\"unknown tag: %s\", tag)
 	}
 }
@@ -120,18 +180,26 @@ func main() {
 		packageName = "NULL"
 	}
 
+	// Initialize template, no point to continue if there is a problem with it.
+	ut, err := template.
+		New("LookupFieldByTag").
+		Funcs(map[string]interface{}{
+			"ReturnValue": ReturnValue,
+		}).
+		Parse(templateStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse template string: %s", err)
+		os.Exit(1)
+	}
+
+	// Process fields.
 	fields, err := getFields(transactions.SignedTxnInBlock{})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to get fields for struct: %s", err)
 		os.Exit(1)
 	}
 
-	ut, err := template.New("LookupFieldByTag").Parse(templateStr)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to parse template string: %s", err)
-		os.Exit(1)
-	}
-
+	// Setup writer to stdout or file.
 	var outputWriter io.Writer
 	if outputFilepath == "" {
 		outputWriter = os.Stdout
@@ -145,106 +213,19 @@ func main() {
 		outputWriter = fout
 	}
 
+	// Prepare template inputs.
 	data := struct {
 		StructFields map[string]StructField
-		InputName    string
 		PackageName  string
 	}{
 		StructFields: fields,
-		InputName:    InputName,
 		PackageName:  packageName,
 	}
+
+	// Process template and write results.
 	err = ut.Execute(outputWriter, data)
-}
-
-/*
-// usage:
-// go run generate.go packagename outputfile
-func main() {
-
-	var packageName string
-	var outputFilepath string
-
-	if len(os.Args) == 3 {
-		packageName = os.Args[1]
-		outputFilepath = os.Args[2]
-	}
-
-	if packageName == "" {
-		packageName = "NULL"
-	}
-
-	output := getFields(transactions.SignedTxnInBlock{})
-
-	var err error
-	var bb bytes.Buffer
-
-	initialStr := `// Code generated via go generate. DO NOT EDIT.
-
-package %s
-
-import (
-	"fmt"
-
-	"github.com/algorand/go-algorand/data/transactions"
-)
-
-// SignedTxnFunc takes a tag and associated SignedTxnInBlock and returns the value
-// referenced by the tag.  An error is returned if the tag does not exist
-func SignedTxnFunc(tag string, input *transactions.SignedTxnInBlock) (interface{}, error) {
-
-`
-	_, err = fmt.Fprintf(&bb, initialStr, packageName)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to write to %s: %v\n", outputFilepath, err)
+		fmt.Fprintf(os.Stderr, "Template execute failure: %s", err)
 		os.Exit(1)
-	}
-
-	keys := []string{}
-
-	for k := range output {
-		keys = append(keys, k)
-	}
-
-	sort.Strings(keys)
-
-	_, err = fmt.Fprintf(&bb, "switch tag {\n")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to write to %s: %v\n", outputFilepath, err)
-		os.Exit(1)
-	}
-
-	for _, k := range keys {
-		fmt.Fprintf(&bb, "case \"%s\":\nreturn &input.%s, nil\n", k, output[k].Path)
-	}
-
-	//nolint:govet
-	_, err = bb.WriteString("default:\n" +
-		"return nil, fmt.Errorf(\"unknown tag: %s\", tag)\n" +
-		"}\n}")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to write to %s: %v\n", outputFilepath, err)
-		os.Exit(1)
-	}
-
-	bbuf, err := format.Source(bb.Bytes())
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "formatting error: %v", err)
-		os.Exit(1)
-	}
-
-	outputStr := string(bbuf)
-
-	if outputFilepath == "" {
-		fmt.Printf("%s", outputStr)
-	} else {
-		fout, err := os.Create(outputFilepath)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "cannot open %s for writing: %v\n", outputFilepath, err)
-			os.Exit(1)
-		}
-		defer fout.Close()
-		fmt.Fprintf(fout, "%s", outputStr)
 	}
 }
-*/
