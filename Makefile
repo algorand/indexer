2df365b7d8bd99deabdfdb62b974fdf04b6d1d66
Eric Warehime <eric.warehime@gmail.com>
2023-05-31 14:34:18 -0700

    Update

--- Makefile
+++ Makefile
@@ -12,11 +12,8 @@ endif
 export GOPATH := $(shell go env GOPATH)
 GOPATH1 := $(firstword $(subst :, ,$(GOPATH)))
 
-# TODO: ensure any additions here are mirrored in misc/release.py
 GOLDFLAGS += -X github.com/algorand/indexer/version.Hash=$(shell git log -n 1 --pretty="%H")
-GOLDFLAGS += -X github.com/algorand/indexer/version.Dirty=$(if $(filter $(strip $(shell git status --porcelain|wc -c)), "0"),,true)
 GOLDFLAGS += -X github.com/algorand/indexer/version.CompileTime=$(shell date -u +%Y-%m-%dT%H:%M:%S%z)
-GOLDFLAGS += -X github.com/algorand/indexer/version.GitDecorateBase64=$(shell git log -n 1 --pretty="%D"|base64|tr -d ' \n')
 GOLDFLAGS += -X github.com/algorand/indexer/version.ReleaseVersion=$(shell cat .version)
 
 COVERPKG := $(shell go list ./...  | grep -v '/cmd/' | egrep -v '(testing|test|mocks)$$' |  paste -s -d, - )
@@ -29,7 +26,7 @@ all: cmd/algorand-indexer/algorand-indexer idb/postgres/internal/schema/setup_po
 
 
 cmd/algorand-indexer/algorand-indexer: idb/postgres/internal/schema/setup_postgres_sql.go
-	cd cmd/algorand-indexer && go build -ldflags="${GOLDFLAGS}"
+	cd cmd/algorand-indexer && go build -ldflags='${GOLDFLAGS}'
 
 idb/postgres/internal/schema/setup_postgres_sql.go:	idb/postgres/internal/schema/setup_postgres.sql
 	cd idb/postgres/internal/schema && go generate
--- version/strings.go
+++ version/strings.go
@@ -1,10 +1,7 @@
 package version
 
 import (
-	"encoding/base64"
 	"fmt"
-	"regexp"
-	"strings"
 )
 
 // These are targets for compiling in build information.
@@ -14,15 +11,8 @@ var (
 	// Hash Git commit hash. Output of `git log -n 1 --pretty="%H"`
 	Hash string
 
-	// Dirty "true" or ""
-	// A release build should have no modified files and no unknown files.
-	Dirty string
-
 	// CompileTime YYYY-mm-ddTHH:MM:SS+ZZZZ
 	CompileTime string
-
-	// GitDecorateBase64 Decorations of latest commit which may include tags. Output of `git log -n 1 --pretty="%D"|base64`
-	GitDecorateBase64 string
 
 	// ReleaseVersion What was in /.version when this was compiled.
 	ReleaseVersion string
@@ -33,38 +23,19 @@ const UnknownVersion = "(unknown version)"
 
 // Version the binary version.
 func Version() string {
-	// parse "tag: 1.2.3" out of the result of `git log -n 1 --pretty="%D"|base64`
-	if len(GitDecorateBase64) == 0 {
+	if ReleaseVersion == "" {
 		return UnknownVersion
 	}
-	b, err := base64.StdEncoding.DecodeString(GitDecorateBase64)
-	if err != nil {
-		return fmt.Sprintf("compiled with bad GitDecorateBase64, %s", err.Error())
-	}
-	tre := regexp.MustCompile(`tag:\s+([^,]+)`)
-	m := tre.FindAllStringSubmatch(string(b), -1)
-	if m == nil {
-		return UnknownVersion
-	}
-	for _, group := range m {
-		if len(group[1]) > 0 {
-			return strings.TrimSpace(group[1])
-		}
-	}
-	return UnknownVersion
+	return ReleaseVersion
 }
 
 // LongVersion the long form of the binary version.
 func LongVersion() string {
-	dirtyStr := ""
-	if (len(Dirty) > 0) && (Dirty != "false") {
-		dirtyStr = " (modified)"
-	}
 	tagVersion := Version()
 	if tagVersion == UnknownVersion {
 		tagVersion = fmt.Sprintf("%s-dev.unknown", ReleaseVersion)
 	} else if tagVersion != ReleaseVersion {
 		tagVersion = fmt.Sprintf("dev release build .version=%s tag=%s", ReleaseVersion, tagVersion)
 	}
-	return fmt.Sprintf("%s compiled at %s from git hash %s%s", tagVersion, CompileTime, Hash, dirtyStr)
+	return fmt.Sprintf("%s compiled at %s from git hash %s", tagVersion, CompileTime, Hash)
 }
