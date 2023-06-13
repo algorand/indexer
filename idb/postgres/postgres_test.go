9b25978b000201b3e0ed5fd13333f74b3adc8363
Eric Warehime <eric.warehime@gmail.com>
2023-06-13 12:50:13 -0700

    Convert timezones before txn query

--- idb/postgres/postgres_test.go
+++ idb/postgres/postgres_test.go
@@ -74,3 +74,48 @@ func Test_UnknownProtocol(t *testing.T) {
 	require.ErrorContains(t, err, protocol)
 	require.ErrorContains(t, err, "you need to upgrade")
 }
+
+func Test_buildTransactionQueryTime(t *testing.T) {
+	us_east_tz, err := time.LoadLocation("America/New_York")
+	require.NoError(t, err)
+	us_west_tz, err := time.LoadLocation("America/Los_Angeles")
+	require.NoError(t, err)
+	random_date_utc := time.Date(1000, time.December, 25, 1, 2, 3, 4, time.UTC)
+	tests := []struct {
+		name      string
+		arg       idb.TransactionFilter
+		whereArgs []interface{}
+	}{
+		{
+			"BeforeTime UTC to UTC",
+			idb.TransactionFilter{
+				BeforeTime: random_date_utc,
+			},
+			[]interface{}{random_date_utc},
+		},
+		{
+			"AfterTime UTC to UTC",
+			idb.TransactionFilter{
+				AfterTime: random_date_utc,
+			},
+			[]interface{}{random_date_utc},
+		},
+		{
+			"BeforeTime AfterTime Conversion",
+			idb.TransactionFilter{
+				BeforeTime: time.Date(1000, time.December, 25, 1, 2, 3, 4, us_east_tz),
+				AfterTime:  time.Date(1000, time.December, 25, 1, 2, 3, 4, us_west_tz),
+			},
+			[]interface{}{
+				time.Date(1000, time.December, 25, 1, 2, 3, 4, us_east_tz).UTC(),
+				time.Date(1000, time.December, 25, 1, 2, 3, 4, us_west_tz).UTC(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, whereArgs, _ := buildTransactionQuery(tt.arg)
+			require.Equal(t, whereArgs, tt.whereArgs)
+		})
+	}
+}
