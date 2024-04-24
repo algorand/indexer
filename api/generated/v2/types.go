466ca2cc91c8814ebea83f17b739356dc3a5296e
John Jannotti <jannotti@gmail.com>
2024-04-24 14:40:56 -0400

    return absent accounts in REST API

--- api/generated/v2/types.go
+++ api/generated/v2/types.go
@@ -695,6 +695,9 @@ type OnCompletion string
 
 // ParticipationUpdates Participation account data that needs to be checked/acted on by the network.
 type ParticipationUpdates struct {
+	// AbsentParticipationAccounts \[partupabs\] a list of online accounts that need to be suspended.
+	AbsentParticipationAccounts *[]string `json:"absent-participation-accounts,omitempty"`
+
 	// ExpiredParticipationAccounts \[partupdrmv\] a list of online accounts that needs to be converted to offline since their participation key expired.
 	ExpiredParticipationAccounts *[]string `json:"expired-participation-accounts,omitempty"`
 }
