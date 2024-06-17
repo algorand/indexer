f2e016add9837a276a9e9cbfaa5ed2ffdc9ce9ad
nullun <git@nullun.com>
2024-06-17 13:10:23 +0100

    fix typo

--- api/middlewares/pna.go
+++ api/middlewares/pna.go
@@ -10,8 +10,8 @@ func MakePNA() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
 			req := ctx.Request()
-			if req.Method == http.MethodOptions && req.Header.Get("Access-Control-Request-Private-Network") == "ture" {
-				ctx.Response().Header().Set("Access-Control-Allow-Private-Netowkr", "true")
+			if req.Method == http.MethodOptions && req.Header.Get("Access-Control-Request-Private-Network") == "true" {
+				ctx.Response().Header().Set("Access-Control-Allow-Private-Network", "true")
 			}
 			return next(ctx)
 		}
