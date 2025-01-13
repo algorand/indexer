084577338ad4882f5797b3e1b30b84718ad40333
Gary Malouf <982483+gmalouf@users.noreply.github.com>
2025-01-13 16:04:09 -0500

    Rewind: Re-enable Rewinding (#1645)
    * Revert "Disable rewinding and reject the  query param on account lookups and searches. (#1630)"
    
    This reverts commit 249016cf51cf9c3aecf37c6fc8c09774523f02bc. The synthetic transaction implementation of payouts in the indexer should allow balances retrieved via rewind to calculate as before.
    
    * GCI lint warning fix.
    
    * Add HeartbeatTxn to be ignored during rewinding.

---
+++ accounting/rewind_test.go
@@ -0,0 +1,80 @@
+package accounting
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	models "github.com/algorand/indexer/v3/api/generated/v2"
+	"github.com/algorand/indexer/v3/idb"
+	"github.com/algorand/indexer/v3/idb/mocks"
+	"github.com/algorand/indexer/v3/types"
+
+	sdk "github.com/algorand/go-algorand-sdk/v2/types"
+)
+
+func TestBasic(t *testing.T) {
+	var a sdk.Address
+	a[0] = 'a'
+
+	account := models.Account{
+		Address:                     a.String(),
+		Amount:                      100,
+		AmountWithoutPendingRewards: 100,
+		Round:                       8,
+	}
+
+	txnRow := idb.TxnRow{
+		Round: 7,
+		Txn: &sdk.SignedTxnWithAD{
+			SignedTxn: sdk.SignedTxn{
+				Txn: sdk.Transaction{
+					Type: sdk.PaymentTx,
+					PaymentTxnFields: sdk.PaymentTxnFields{
+						Receiver: a,
+						Amount:   sdk.MicroAlgos(2),
+					},
+				},
+			},
+		},
+	}
+
+	ch := make(chan idb.TxnRow, 1)
+	ch <- txnRow
+	close(ch)
+	var outCh <-chan idb.TxnRow = ch
+
+	db := &mocks.IndexerDb{}
+	db.On("GetSpecialAccounts", mock.Anything).Return(types.SpecialAddresses{}, nil)
+	db.On("Transactions", mock.Anything, mock.Anything).Return(outCh, uint64(8))
+
+	account, err := AccountAtRound(context.Background(), account, 6, db)
+	assert.NoError(t, err)
+
+	assert.Equal(t, uint64(98), account.Amount)
+}
+
+// Test that when idb.Transactions() returns stale data the first time, we return an error.
+func TestStaleTransactions1(t *testing.T) {
+	var a sdk.Address
+	a[0] = 'a'
+
+	account := models.Account{
+		Address: a.String(),
+		Round:   8,
+	}
+
+	ch := make(chan idb.TxnRow)
+	var outCh <-chan idb.TxnRow = ch
+	close(ch)
+
+	db := &mocks.IndexerDb{}
+	db.On("GetSpecialAccounts", mock.Anything).Return(types.SpecialAddresses{}, nil)
+	db.On("Transactions", mock.Anything, mock.Anything).Return(outCh, uint64(7)).Once()
+
+	account, err := AccountAtRound(context.Background(), account, 6, db)
+	assert.True(t, errors.As(err, &ConsistencyError{}), "err: %v", err)
+}
