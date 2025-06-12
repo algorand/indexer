b83805117377abe20e104409f571504661739f59
Gary Malouf <982483+gmalouf@users.noreply.github.com>
2025-06-12 17:04:24 -0400

    Bump to latest snapshot go-algorand-sdk, update for type def updates.

--- util/test/account_testutil.go
+++ util/test/account_testutil.go
@@ -59,7 +59,8 @@ func DecodeAddressOrPanic(addr string) sdk.Address {
 // ArbitraryString should be used to generate a pseudo-random string to put in the Note field of a Txn Header.
 // This is necessary to ensure the hash of any two txns used in tests are never the same.
 func ArbitraryString() []byte {
-	arb := make([]byte, config.MaxTxnNoteBytes)
+	future := config.Consensus[protocol.ConsensusFuture]
+	arb := make([]byte, future.MaxTxnNoteBytes)
 	_, err := rand.Read(arb)
 	if err != nil {
 		panic("rand.Read error")
